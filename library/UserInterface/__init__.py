684ef0c1ac7b85d7c04b40cb926c05400da0fe7f
Mark Geurts <mark.w.geurts@gmail.com>
2018-04-13 17:13:26 -0500

    Added question box

--- library/UserInterface/__init__.py
+++ library/UserInterface/__init__.py
@@ -2,5 +2,6 @@ from ProgressBar import ProgressBar
 from CommonDialog import CommonDialog
 from MessageBox import MessageBox
 from MessageBox import WarningBox
+from MessageBox import QuestionBox
 from InputDialog import InputDialog
 from ScriptStatus import ScriptStatus
