70a49a9b71b0096ce16396260c8710bf4e1fa8af
Mark Geurts <mark.w.geurts@gmail.com>
2018-04-20 16:48:31 -0500

    Narrows storage class list

--- library/DicomExport.py
+++ library/DicomExport.py
@@ -104,12 +104,12 @@ def send(case,
     # Load energy filters for selected machine
     if filters is not None and 'energy' in filters and beamset is not None:
         energy_list = energies(beamset, machine)
-
+        print energy_list
     else:
         energy_list = None
 
     # Establish connections with all SCP destinations
-    if not bar:
+    if bar:
         bar = UserInterface.ProgressBar(text='Establishing connection to DICOM destinations',
                                         title='Export Progress',
                                         marquee=True)
@@ -126,9 +126,9 @@ def send(case,
             # Throw errors unless C-ECHO responds
             if assoc.is_established:
                 logging.debug('Association accepted by the peer')
-                status = assoc.send_c_echo()
+                response = assoc.send_c_echo()
                 assoc.release()
-                logging.debug('C-ECHO Response: 0x{0:04x}'.format(status.Status))
+                logging.debug('C-ECHO Response: 0x{0:04x}'.format(response.Status))
 
             elif assoc.is_rejected and not ignore_errors:
                 if isinstance(bar, UserInterface.ProgressBar):
@@ -212,10 +212,10 @@ def send(case,
                     # If applying a machine filter
                     if machine is not None:
                         if b.TreatmentMachineName != machine:
-                            logging.debug('Updating {} on beam {} to {}'.format(
-                                str(b.data_element('TreatmentMachineName').tag), b.BeamNumber, machine))
                             b.TreatmentMachineName = machine
                             edits.append(str(b.data_element('TreatmentMachineName').tag))
+                            logging.debug('Updating {} on beam {} to {}'.format(
+                                str(b.data_element('TreatmentMachineName').tag), b.BeamNumber, machine))
 
                     # If applying an energy filter
                     if energy_list is not None and hasattr(b, 'ControlPointSequence'):
@@ -224,23 +224,23 @@ def send(case,
                                 e = float(re.sub('\D+', '', energy_list[c.NominalBeamEnergy]))
                                 m = re.sub('\d+', '', energy_list[c.NominalBeamEnergy])
                                 if c.NominalBeamEnergy != e:
-                                    logging.debug('Updating {} on beam {}, CP {} to {}'.format(str(
-                                        c.data_element('NominalBeamEnergy').tag), b.BeamNumber, c.ControlPointIndex, e))
                                     c.NominalBeamEnergy = e
                                     edits.append(str(c.data_element('NominalBeamEnergy').tag))
+                                    logging.debug('Updating {} on beam {}, CP {} to {}'.format(str(
+                                        c.data_element('NominalBeamEnergy').tag), b.BeamNumber, c.ControlPointIndex, e))
 
                                 # If a non-standard fluence, add mode ID and NON_STANDARD flag
                                 if not hasattr(b, 'FluenceModeID') or b.FluenceModeID != m:
-                                    logging.debug('Updating {} on beam {}, CP {} to {}'.format(
-                                        str(b.data_element('FluenceModeID').tag), b.BeamNumber, c.ControlPointIndex, m))
                                     b.FluenceModeID = m
                                     edits.append(str(b.data_element('FluenceModeID').tag))
+                                    logging.debug('Updating {} on beam {}, CP {} to {}'.format(
+                                        str(b.data_element('FluenceModeID').tag), b.BeamNumber, c.ControlPointIndex, m))
                                     if m != '':
+                                        b.FluenceMode = 'NON_STANDARD'
+                                        edits.append(str(b.data_element('FluenceMode').tag))
                                         logging.debug('Updating {} on beam {}, CP {} to {}'.format(
                                             str(b.data_element('FluenceMode').tag), b.BeamNumber, c.ControlPointIndex,
                                             'NON_STANDARD'))
-                                        b.FluenceMode = 'NON_STANDARD'
-                                        edits.append(str(b.data_element('FluenceMode').tag))
 
             # If no edits are needed, copy the file to the modified directory
             if len(edits) == 0:
@@ -349,25 +349,30 @@ def send(case,
 
                 # Send to SCP via pynetdicom3
                 if len({'host', 'aet', 'port'}.difference(info)) == 0:
-                    ae = pynetdicom3.AE(scu_sop_class=pynetdicom3.StorageSOPClassList,
+                    ae = pynetdicom3.AE(scu_sop_class=['1.2.840.10008.5.1.4.1.1.481.5',
+                                                       '1.2.840.10008.5.1.4.1.1.481.3',
+                                                       '1.2.840.10008.5.1.4.1.1.481.2',
+                                                       '1.2.840.10008.5.1.4.1.1.2'],
                                         ae_title=local_AET,
-                                        port=local_port)
+                                        port=local_port,
+                                        transfer_syntax=['1.2.840.10008.1.2'])
+
                     assoc = ae.associate(info['host'], int(info['port']), ae_title=info['aet'])
                     if assoc.is_established:
-                        status = assoc.send_c_store(dataset=ds,
-                                                    msg_id=1,
-                                                    priority=0,
-                                                    originator_aet='RayStation',
-                                                    originator_id=None)
+                        response = assoc.send_c_store(dataset=ds,
+                                                      msg_id=1,
+                                                      priority=0,
+                                                      originator_aet=None,
+                                                      originator_id=None)
                         assoc.release()
-                        logging.info('{0} -> {1} C-STORE status: 0x{2:04x}'.format(m, d, status.Status))
-                        if status.Status != 0:
+                        logging.info('{0} -> {1} C-STORE status: 0x{2:04x}'.format(m, d, response.Status))
+                        if response.Status != 0:
                             status = False
                             if not ignore_errors:
                                 if isinstance(bar, UserInterface.ProgressBar):
                                     bar.close()
 
-                                raise IOError('C-STORE ERROR: 0x{2:04x}'.format(m, d, status.Status))
+                                raise IOError('C-STORE ERROR: 0x{2:04x}'.format(m, d, response.Status))
 
                     elif assoc.is_rejected and not ignore_errors:
                         if isinstance(bar, UserInterface.ProgressBar):
@@ -471,15 +476,15 @@ def energies(beamset=None, machine=None):
 
         # If the filter is a machine and energy filter, verify the machine matches
         if 'type' in c.attrib and c.attrib['type'] == 'machine/energy' and \
-                c.findall('from/machine')[0] == beamset.MachineReference.MachineName:
+                c.findall('from/machine')[0].text == beamset.MachineReference.MachineName:
             for t in c.findall('to'):
-                if machine is None or t.findall('machine')[0] == machine:
-                    energy_list[float(c.findall('from/energy')[0])] = t.findall('energy')[0].text
+                if machine is None or t.findall('machine')[0].text == machine:
+                    energy_list[float(c.findall('from/energy')[0].text)] = t.findall('energy')[0].text
 
         # Otherwise, if only an energy filter
         elif 'type' in c.attrib and c.attrib['type'] == 'energy':
             for t in c.findall('to'):
-                energy_list[float(c.findall('from/energy')[0])] = t.findall('energy')[0].text
+                energy_list[float(c.findall('from/energy')[0].text)] = t.findall('energy')[0].text
 
     return energy_list
 
