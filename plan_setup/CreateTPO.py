a7faa5a95f9fa501964c93b7ddfadce83059aaf2
Mark Geurts <mark.w.geurts@gmail.com>
2018-04-27 16:38:17 -0500

    Drafted proposal for protocol, goalset XMLs

---
+++ plan_setup/CreateTPO.py
@@ -0,0 +1,107 @@
+""" Approve Contours and Create TPO
+
+    This script is for physicians to approve their contour set for planning and to
+    generate a Treatment Planning Order (TPO) for import into ARIA. As part of TPO
+    creation, this script will also add clinical goals and initialize the treatment
+    plan(s).
+
+    Finally, dose is computed and then exported after each beamset is created. The DICOM
+    export path is set in the configuration settings below. The option to only create
+    beams is available by un-checking the "calculate" and "export" runtime options from
+    the configuration dialog box.
+
+    This program is free software: you can redistribute it and/or modify it under the
+    terms of the GNU General Public License as published by the Free Software Foundation,
+    either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+    PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this
+    program. If not, see <http://www.gnu.org/licenses/>.
+    """
+
+__author__ = 'Mark Geurts'
+__contact__ = 'mark.w.geurts@gmail.com'
+__version__ = '1.0.0'
+__license__ = 'GPLv3'
+__help__ = 'https://github.com/mwgeurts/ray_scripts/wiki/Create-TPO'
+__copyright__ = 'Copyright (C) 2018, University of Wisconsin Board of Regents'
+
+# Import packages
+import sys
+import os
+import connect
+import UserInterface
+import logging
+import time
+import xml.etree.ElementTree
+
+# Define the protocol XML directory
+protocol_folder = '../protocols'
+
+
+def main():
+
+    # Get current patient and case
+    try:
+        patient = connect.get_current('Patient')
+        case = connect.get_current('Case')
+        patient.Save()
+
+    except Exception:
+        UserInterface.WarningBox('This script requires a patient to be loaded')
+        sys.exit('This script requires a patient to be loaded')
+
+    # Start timer
+    tic = time.time()
+
+    # Start script status
+    status = UserInterface.ScriptStatus(steps=['Enter plan information for TPO',
+                                               'Clean up structure list',
+                                               'Approve structures',
+                                               'Initialize plan(s)',
+                                               'Set clinical goals',
+                                               'Generate TPO'],
+                                        docstring=__doc__,
+                                        help=__help__)
+
+    # Display input dialog box for TPO
+    status.next_step(text='In order to approve the plan and create a TPO, please fill out the displayed form ' +
+                          'with information about the plan. Once completed, click OK to continue.')
+
+    # Search protocol list, parsing each XML file for protocols
+    protocols = {}
+    goalsets = {}
+    for f in os.listdir(os.path.join(os.path.dirname(__file__), protocol_folder)):
+        if f.endswith('.xml'):
+            fxml = xml.etree.ElementTree.parse(os.path.join(os.path.dirname(__file__), f))
+            if fxml.getroot().tag == 'protocol':
+                protocols[fxml.getroot().attrib['name']] = fxml.getroot()
+
+            elif fxml.getroot().tag == 'goalsets':
+                for g in fxml.findall('/set'):
+                    goalsets[g.attrib['name']] = g
+
+
+
+
+
+
+
+
+
+
+
+
+    # Finish up
+    patient.Save()
+    time.sleep(1)
+    logging.debug('Script completed successfully in {:.3f} seconds'.format(time.time() - tic))
+    status.finish('Script completed successfully. You may now import the saved TPO into ARIA and notify dosimetry ' +
+                  'that this case is ready for planning.')
+
+
+if __name__ == '__main__':
+    main()
