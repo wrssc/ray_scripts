50dae9fb30c57eefd77c515cb53653d04ef3e8e6
Adam Bayliss <rabayliss@wisc.edu>
2018-08-25 21:02:45 -0500

    Debug
    Adding first protocol flag to fix indent issue.  adding mains to function

--- general/GenerateGoalTemplates.py
+++ general/GenerateGoalTemplates.py
@@ -8,11 +8,15 @@ import xml.etree.ElementTree
 import connect
 import Goals
 
-protocol = r'../protocols/SampleGoal.xml'
-#protocol_file = r'SampleGoal.xml'
-protocol_name = os.path.join(os.path.dirname(__file__), protocol)
+def main():
+    protocol = r'../protocols/SampleGoal.xml'
+    #protocol_file = r'SampleGoal.xml'
+    protocol_name = os.path.join(os.path.dirname(__file__), protocol)
 
-tree = xml.etree.ElementTree.parse(protocol_name)
-for g in tree.findall('//goals/roi'):
-    print 'Adding goal ' + Goals.print_goal(g, 'xml')
-    Goals.add_goal(g, connect.get_current('Plan'))
+    tree = xml.etree.ElementTree.parse(protocol_name)
+    for g in tree.findall('//goals/roi'):
+        print 'Adding goal ' + Goals.print_goal(g, 'xml')
+        Goals.add_goal(g, connect.get_current('Plan'))
+
+if __name__ ==  'main':
+    main()
