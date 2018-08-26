0d398865ddffc3fe9105699a8043159966199c41
Adam Bayliss <rabayliss@wisc.edu>
2018-08-25 20:10:12 -0500

    Updated file with correct path
    Changed header to show up correctly, added a simple string to the call to xml...parse method

--- general/GenerateGoalTemplates.py
+++ general/GenerateGoalTemplates.py
@@ -1,8 +1,15 @@
+'''Generate Goal Templates
+This function is a stub for what will eventually be all goals relevant to
+standard TPO's
+'''
 import xml.etree.ElementTree
 import connect
 import Goals
 
-tree = xml.etree.ElementTree.parse('SampleGoal.xml')
+protocol_folder = r'../protocols'
+protocol_file = protocol_folder+'SampleGoal.xml'
+#os.path.join(os.path.dirname(__file__), protocol_folder))
+tree = xml.etree.ElementTree.parse(protocol_file)
 for g in tree.findall('//goals/roi'):
     print 'Adding goal ' + Goals.print_goal(g, 'xml')
     Goals.add_goal(g, connect.get_current('Plan'))
