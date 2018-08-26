746cd5c64dcd5ca2248fd5ae3ce8b714124c6976
Adam Bayliss <rabayliss@wisc.edu>
2018-08-25 20:39:18 -0500

    Debug
    resolving errors in filepath

--- general/GenerateGoalTemplates.py
+++ general/GenerateGoalTemplates.py
@@ -2,14 +2,15 @@
 This function is a stub for what will eventually be all goals relevant to
 standard TPO's
 '''
+import sys
+import os
 import xml.etree.ElementTree
 import connect
 import Goals
-import os
 
-protocol_folder = r'../protocols'
-protocol_file = 'SampleGoal.xml'
-protocol_path = os.path.join(os.path.dirname(__file__), protocol_folder)
+protocol_folder = r'../protocols/'
+protocol_file = r'SampleGoal.xml'
+protocol_path = os.path.join(os.path.dirname(__file__), protocol_folder+protocol_file)
 
 tree = xml.etree.ElementTree.parse(protocol_path+protocol_file)
 for g in tree.findall('//goals/roi'):
