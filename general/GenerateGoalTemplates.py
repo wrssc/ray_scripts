707e5b8d49b4b84c3b12da11e039955bfdc1cfe8
Adam Bayliss <rabayliss@wisc.edu>
2018-08-30 11:22:37 -0500

    Added Planning structure script and repaired Goal Templates

--- general/GenerateGoalTemplates.py
+++ general/GenerateGoalTemplates.py
@@ -17,6 +17,11 @@ def main():
     for g in tree.findall('//goals/roi'):
         print 'Adding goal ' + Goals.print_goal(g, 'xml')
         Goals.add_goal(g, connect.get_current('Plan'))
+    ui = connect.get_current('ui')
+    ui.TitleBar.MenuItem['Plan Optimization'].Click()
+    ui.TitleBar.MenuItem['Plan Optimization'].Popup.MenuItem['Plan Optimization'].Click()
+    ui.Workspace.TabControl['DVH'].TabItem['Clinical Goals'].Select()
+    #ui.ToolPanel.TabItem._Scripting.Select()
 
 if __name__ ==  'main':
     main()
