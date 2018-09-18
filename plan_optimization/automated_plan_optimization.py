26b3eb917e29b17aabc6c35bd664a0a6659aff0a
Adam Bayliss <rabayliss@wisc.edu>
2018-09-18 17:30:33 -0500

    automated optimization dialog
    debuggin

--- plan_optimization/automated_plan_optimization.py
+++ plan_optimization/automated_plan_optimization.py
@@ -156,7 +156,7 @@ def optimize_plan(patient, case, plan, beamset, **optimization_inputs):
 
     # Making the variable status script, arguably move to main()
     status_steps = ['Initializing optimization']
-    for i in range(int(optimization_dialog.values['input7_n_iterations'])):
+    for i in range(maximum_iteration):
         ith_step = 'Executing Iteration:' + str(i + 1)
         status_steps.append([ith_step])
     status_steps.append(['Reduce OAR Dose'])
