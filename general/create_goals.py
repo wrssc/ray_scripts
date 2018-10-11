3e09a86bd71db187ab66f3d5fbc716f50d5cbed8
Adam Bayliss <rabayliss@wisc.edu>
2018-10-11 18:17:29 -0500

    create_goals.py
    Can we return the list of targets from the protocol?  Yes, I know, there's one target specified in the prescriptions, still we need all targets.

--- general/create_goals.py
+++ general/create_goals.py
@@ -30,7 +30,7 @@ def main():
     file_name = 'UWLung_StandardFractionation.xml'
     path_file = os.path.join(os.path.dirname(__file__), protocol_folder, institution_folder, file_name)
     path_protocols = os.path.join(os.path.dirname(__file__), protocol_folder, institution_folder)
-    # Get current patient, case, and exam
+    # Get current patient, case, exam, and plan
     try:
         patient = connect.get_current('Patient')
         case = connect.get_current('Case')
@@ -43,7 +43,8 @@ def main():
         sys.exit('This script requires a patient and plan to be loaded')
 
     # TODO: replace all the hardcoded options with a user interface prompt
-
+    # Without adding another attribute to the goals list, here's what we need to do
+    targets = ['PTV','ITV','GTV']
     tpo = UserInterface.TpoDialog()
     tpo.load_protocols(path_protocols)
 
@@ -67,20 +68,24 @@ def main():
     number_targets = input_dialog.values['input0']
     input_protocol = input_dialog.values['input1']
 
-    #path_file = path_file = os.path.join(os.path.dirname(__file__),
+    # To load the xml from file directly, without use of the TPO load:
+    # path_file = path_file = os.path.join(os.path.dirname(__file__),
     #                                     protocol_folder, institution_folder, file_name)
-    #tree = xml.etree.ElementTree.parse(path_file)
-    # Need to load the value of this key
-    root = tpo.protocols[input_protocol]
-   # tree = tpo.protocols[input_protocol]
-   # root = tree.getroot()
-
+    # tree = xml.etree.ElementTree.parse(path_file)
+    # tree = tpo.protocols[input_protocol]
+    # root = tree.getroot()
+    root = tpo.protocols[input_dialog.values['input1']]
+    protocol_targets = []
     for g in root.findall('./goals/roi'):
-        if g.find('name').text == ptv_name and "%" in g.find('dose').attrib['units']:
-            g.find('dose').attrib = "Gy"
-            g.find('dose').text = str(ptv_md_dose)
-        logging.debug('create_goals.py: Adding goal ' + Goals.print_goal(g, 'xml'))
-        Goals.add_goal(g, connect.get_current('Plan'))
+        if g.find('name').text in targets and g.find('name').text not in protocol_targets:
+            protocol_targets.append(g.find('name').text)
+
+    print "protocol contains {} targets called: {}".format(len(protocol_targets),protocol_targets)
+ #       if g.find('name').text == ptv_name and "%" in g.find('dose').attrib['units']:
+ #           g.find('dose').attrib = "Gy"
+ #           g.find('dose').text = str(ptv_md_dose)
+ #       logging.debug('create_goals.py: Adding goal ' + Goals.print_goal(g, 'xml'))
+ #       Goals.add_goal(g, connect.get_current('Plan'))
 
 
 if __name__ == '__main__':
