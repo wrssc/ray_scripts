c52fa611e579b25a846ef432d3eca09fba63592b
Adam Bayliss <rabayliss@wisc.edu>
2018-10-12 15:34:24 -0500

    debug
    create_goals

--- general/create_goals.py
+++ general/create_goals.py
@@ -111,11 +111,12 @@ def main():
         final_required.append(k_dose)
     # Grab the number of inputs so far and start new input
     i = len(final_inputs) + 1
-   ## for t in protocol_targets:
-   ##     print t
-   ##     final_inputs[str(i)] = 'Match protocol target:' + str(t) + ' to one of these'
-   ##     final_options[str(i)] = target_matches
-   ##     i += 1
+    for t in protocol_targets:
+        print t
+        k_name = str(i)
+        final_inputs[k_name] = 'Match protocol target:' + str(t) + ' to one of these'
+        final_options[k_name] = target_matches
+        i += 1
     # Loop over user identified targets that have no corresponding match.
     for k, v in final_inputs.iteritems():
         logging.debug("create_goals.py: Inputs created as name, value: {} : {}".format(
