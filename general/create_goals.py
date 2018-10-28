0e8a0ea98413e347bec54f7a354c231363c502d1
Adam Bayliss <rabayliss@wisc.edu>
2018-10-28 16:37:04 -0500

    create_goals
    debug

--- general/create_goals.py
+++ general/create_goals.py
@@ -155,25 +155,25 @@ def main():
     for s in goal_locations:
         for g in s:
             g_name = g.find('name').text
-                # Priorities should be even for targets and append unique elements only
-                # into the protocol_targets list
-                if int(g.find('priority').text) % 2 == 0 and g_name not in protocol_targets:
-                    protocol_targets.append(g_name)
-                    k = str(i)
-                    # Python doesn't sort lists....
-                    k_name = k.zfill(2) + 'Aname_' + g_name
-                    k_dose = k.zfill(2) + 'Bdose_' + g_name
-                    final_inputs[k_name] = 'Match a plan target to ' + g_name
-                    final_options[k_name] = plan_targets
-                    final_datatype[k_name] = 'combo'
-                    final_required.append(k_name)
-                    final_inputs[k_dose] = 'Provide dose for protocol target: ' + g_name + ' Dose in cGy'
-                    final_required.append(k_dose)
-                    i += 1
-                    # Exact matches get an initial guess in the dropdown
-                    for t in plan_targets:
-                        if g_name == t:
-                            final_initial[k_name] = t
+            # Priorities should be even for targets and append unique elements only
+            # into the protocol_targets list
+            if int(g.find('priority').text) % 2 == 0 and g_name not in protocol_targets:
+                protocol_targets.append(g_name)
+                k = str(i)
+                # Python doesn't sort lists....
+                k_name = k.zfill(2) + 'Aname_' + g_name
+                k_dose = k.zfill(2) + 'Bdose_' + g_name
+                final_inputs[k_name] = 'Match a plan target to ' + g_name
+                final_options[k_name] = plan_targets
+                final_datatype[k_name] = 'combo'
+                final_required.append(k_name)
+                final_inputs[k_dose] = 'Provide dose for protocol target: ' + g_name + ' Dose in cGy'
+                final_required.append(k_dose)
+                i += 1
+                # Exact matches get an initial guess in the dropdown
+                for t in plan_targets:
+                    if g_name == t:
+                        final_initial[k_name] = t
             # Add a quick check if the contour exists in RS
             if int(g.find('priority').text) % 2:
                 if not any(r.Name == g_name for r in
