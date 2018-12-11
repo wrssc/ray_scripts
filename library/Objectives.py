78b764a0564869d61303c67ade0d5ef5673fed3c
Adam Bayliss <rabayliss@wisc.edu>
2018-12-11 15:50:15 -0600

    Objectives and Sample xml
    Added basic objective functions and a sample xml file that has one of each objective function.

--- library/Objectives.py
+++ library/Objectives.py
@@ -4,153 +4,130 @@
     add the objectives to RayStation. Contains functions for reassignment of an
     objective when the target name is not matched
 """
+import sys
+import os
+import logging
+import datetime
+import xml.etree.ElementTree
+import connect
 
-def add_objective(function_type,
+
+def select_objectives(folder=None,filename=None):
+    """
+
+    :param filename: os joined protocol name
+    :param folder: folder from os to search within
+    :return: tree: elementtree from xml file
+    """
+    if filename:
+        tree = xml.etree.ElementTree.parse(filename)
+    elif folder:
+        # Search protocol list, parsing each XML file for protocols and goalsets
+        logging.debug('Searching folder {} for protocols, goal sets'.format(folder))
+        for f in os.listdir(folder):
+            if f.endswith('.xml'):
+                tree = xml.etree.ElementTree.parse(os.path.join(folder, f))
+    return tree
+
+
+def add_objective(plan,
+                  beamset,
+                  function_type,
                   roi_name,
-                  dose,
-                  is_constraint,
-                  restrict_all_beams,
-                  restrict_beam,
-                  robust,
-                  restrict_beamset,
-                  use_rbe,
-                  volume=volume):
+                  robust=False,
+                  is_constraint=False,
+                  dose=None,
+                  volume=None,
+                  eud_a = None,
+                  restrict_beamset=None,
+                  low_dose_dist=None,
+                  high_dose=None,
+                  low_dose=None,
+                  adapt_dose=False,
+                  ):
     """
     Add objective to RayStation
     :return:
     """
 
-    with CompositeAction('Add Optimization Function'):
-        retval_0 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="MinDose",
-                                                                     RoiName="PTV_p",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None,
-                                                                     IsRobust=False,
-                                                                     RestrictToBeamSet=None,
-                                                                     UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[0].DoseFunctionParameters.DoseLevel = 5000
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_1 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="MaxDose", RoiName="PTV_p",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[1].DoseFunctionParameters.DoseLevel = 5000
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_2 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="MinDvh", RoiName="PTV_p",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[2].DoseFunctionParameters.DoseLevel = 5000
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[2].DoseFunctionParameters.PercentVolume = 30
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_3 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="UniformDose", RoiName="PTV_p",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[3].DoseFunctionParameters.DoseLevel = 5000
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[3].DoseFunctionParameters.PercentVolume = 30
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_4 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="MinEud", RoiName="PTV_p",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[4].DoseFunctionParameters.DoseLevel = 5000
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[4].DoseFunctionParameters.EudParameterA = 3
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_5 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="MaxEud", RoiName="PTV_p",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[5].DoseFunctionParameters.DoseLevel = 5000
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[5].DoseFunctionParameters.EudParameterA = 3
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_6 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="TargetEud", RoiName="GTV",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[6].DoseFunctionParameters.DoseLevel = 5000
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_7 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="TargetEud", RoiName="GTV",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[7].DoseFunctionParameters.DoseLevel = 5000
-
-        # CompositeAction ends
-
-    with CompositeAction('Add Optimization Function'):
-        retval_8 = plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="DoseFallOff", RoiName="GTV",
-                                                                     IsConstraint=False,
-                                                                     RestrictAllBeamsIndividually=False,
-                                                                     RestrictToBeam=None, IsRobust=False,
-                                                                     RestrictToBeamSet=None, UseRbeDose=False)
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[8].DoseFunctionParameters.HighDoseLevel = 6000
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[8].DoseFunctionParameters.LowDoseLevel = 2000
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[8].DoseFunctionParameters.LowDoseDistance = 2
-
-        plan.PlanOptimizations[0].Objective.ConstituentFunctions[
-            8].DoseFunctionParameters.AdaptToTargetDoseLevels = True
-
-        # CompositeAction ends
-
-def load_objectives(protocol_file):
-    """
-    load objectives from an xml file
-    :param protocol_file:
-    :return:
-    """
-
-    # Script recorded 03 Dec 2018
-
-    #   RayStation version: 7.0.0.19
-    #   Selected patient: ...
-
-    from connect import *
-
-    plan = get_current("Plan")
+    # Find current Beamset Number and determine plan optimization
+    OptIndex = 0
+    IndexNotFound = True
+    # In RS, OptimizedBeamSets objects are keyed using the DicomPlanLabel, or Beam Set name.
+    # Because the key to the OptimizedBeamSets presupposes the user knows the PlanOptimizations index
+    # this while loop looks for the PlanOptimizations index needed below by searching for a key
+    # that matches the BeamSet DicomPlanLabel
+    # This can likely be replaced with a list comprehension
+    while IndexNotFound:
+        try:
+            OptName = plan.PlanOptimizations[OptIndex].OptimizedBeamSets[beamset.DicomPlanLabel].DicomPlanLabel
+            IndexNotFound = False
+        except Exception:
+            IndexNotFound = True
+            OptIndex += 1
+    if IndexNotFound:
+        logging.warning("optimize_plan: Beamset optimization for {} could not be found.".format(beamset.DicomPlanLabel))
+        sys.exit("Could not find beamset optimization")
+    else:
+        # Found our index.  We will use a shorthand for the remainder of the code
+        plan_optimization = plan.PlanOptimizations[OptIndex]
+        #plan_optimization_parameters = plan.PlanOptimizations[OptIndex].OptimizationParameters
+        logging.info(
+            'optimize_plan: Optimization found, proceeding with plan.PlanOptimization[{}] for beamset {}'.format(
+                OptIndex, plan_optimization.OptimizedBeamSets[beamset.DicomPlanLabel].DicomPlanLabel
+            ))
 
 
+    retval_0 = plan_optimization.AddOptimizationFunction(FunctionType=function_type,
+                                                  RoiName=roi_name,
+                                                  IsConstraint=is_constraint,
+                                                  RestrictAllBeamsIndividually=False,
+                                                  RestrictToBeam=None,
+                                                  IsRobust=robust,
+                                                  RestrictToBeamSet=restrict_beamset,
+                                                  UseRbeDose=False)
+
+    if dose:
+        retval_0.DoseFunctionParameters.DoseLevel = dose
+    if volume:
+        retval_0.PercentVolume = volume
+    if eud_a:
+        retval_0.EudParameterA = eud_a
+    # It should be noted that the next 4 inputs should be combined into a "type" based argument for the
+    # dose fall off type of optimization option.
+    if high_dose:
+        retval_0.DoseFunctionParameters.HighDoseLevel = high_dose
+    if low_dose:
+        retval_0.DoseFunctionParameters.LowDoseLeve = low_dose
+    if low_dose_dist:
+        retval_0.DoseFunctionParameters.LowDoseDistance = low_dose_dist
+    if adapt_dose:
+        retval_0.DoseFunctionParameters.AdaptToTargetDoseLevels = adapt_dose
+
+
+def main():
+    """ Temp chunk of code to try to open an xml file"""
+    try:
+        patient = connect.get_current('Patient')
+        case = connect.get_current("Case")
+        examination = connect.get_current("Examination")
+        plan = connect.get_current("Plan")
+        beamset = connect.get_current("BeamSet")
+    except:
+        logging.warning("patient, case and examination must be loaded")
+
+    file = 'planning_structs_conventional.xml'
+    #obj = {'function_type': 'MinEud',
+    #       'roi_name': 'PTV1',
+    #       'constraint': False,
+    #       'eud_a': 3,
+    #       'dose': 50}
+    add_objective(plan=plan,
+                  function_type='MinEud',
+                  roi_name='PTV1',
+                  eud_a=3,
+                  dose=50)
+
+
+if __name__ == '__main__':
+    main()
