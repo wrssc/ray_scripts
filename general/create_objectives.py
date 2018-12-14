887590e7d4c67ad31de6d925de69fe24574c0770
Adam Bayliss <rabayliss@wisc.edu>
2018-12-13 18:07:51 -0600

    planning_structs_test_file and create_objectives
    Added all parsing for the add_objectives function

--- general/create_objectives.py
+++ general/create_objectives.py
@@ -25,31 +25,134 @@ def select_objectives(folder=None, filename=None):
         # Search protocol list, parsing each XML file for protocols and goalsets
         logging.debug('Searching folder {} for protocols, goal sets'.format(folder))
         for f in os.listdir(folder):
+        # This guy should prompt the user to find the appropriate file
             if f.endswith('.xml'):
                 tree = xml.etree.ElementTree.parse(os.path.join(folder, f))
     return tree
 
 
-def add_objective(plan,
-                  beamset,
-                  function_type,
-                  roi_name,
-                  weight,
-                  robust=False,
-                  is_constraint=False,
-                  dose=None,
-                  volume=None,
-                  eud_a=None,
-                  restrict_beamset=None,
-                  low_dose_dist=None,
-                  high_dose=None,
-                  low_dose=None,
-                  adapt_dose=False,
-                  ):
+#def add_goal(goal, plan, roi=None, targets=None, exam=None, case=None):
+
+def add_objective(obj, case, plan, beamset, s_roi=None, s_dose=None, s_weight=None, restrict_beamset=None):
     """
-    Add objective to RayStation
-    :return:
+    adds an objective function to the optimization in RayStation after
+    :param obj: child (roi-tag) of an ElementTree - consider mak
+    :param case: RS case
+    :param plan: RS plan
+    :param beamset: RS beamset
+    :param s_roi: substitute roi for the name tag
+    :param s_dose: substitute dose for the dose tag, str dose in Gy, this will be used as
+                    reference dose in Dose Fall Off operations
+    :param s_weight: substitute weight from protocol-defined tag, str weight
+    :param restrict_beamset: if co-optimization is used, this is needed to restrict an objective to a
+                            given beamset
+    :return: (none) although obj is directly modified by this function
     """
+    # Parse the objectives
+    #
+    # If an roi was specified replace the name tag
+    if s_roi:
+        logging.debug("Objective is added for protocol ROI: {} using plan ROI: {}".format(
+            obj.find('name').text, s_roi))
+        roi = s_roi
+        obj.find('name').text = roi
+    else:
+        roi = obj.find('name').text
+    #
+    # Deal with relative or absolute volumes, modify the volume tag
+    # (RayStation only allows relative volume roi's
+    if obj.find('volume').attrib["units"] == "cc":
+        try:
+            t = case.PatientModel.StructureSets[exam.Name]. \
+                RoiGeometries[s_roi]
+            if t.HasContours():
+                roi_vol = t.GetRoiVolume()
+                volume = float(obj.find('volume').text) / roi_vol
+                obj.find('volume').text = str(volume)
+                obj.find('volume').attrib["units"] = "%"
+            else:
+                logging.warning('add_objective: {} has no contours, index undefined'.format(s_roi))
+        except:
+            logging.warning('add_objective: Error getting volume for {}, volume => 0.0'.format(s_roi))
+    elif obj.find('volume').attrib["units"] == "%":
+        volume = float(obj.find('volume').text)
+    # Modify the dose tag if relative
+    if s_dose:
+        logging.debug('add_objective: ROI: {} Protocol dose {} {} substituted with {} Gy'.format(
+            obj.find('name').text, obj.find('dose').text, obj.find('dose').attrib["units"], s_dose))
+        if obj.find('dose').attrib["units"] == "%":
+            obj.find('dose').attrib["units"] = "Gy"
+            obj.find('dose').text = s_dose
+            dose = float(s_dose)
+        else:
+            obj.find('dose').text = float(s_dose)
+    #
+    # Read the weight variable
+    if s_weight:
+        logging.debug('add_objective: ROI: {} Protocol weight {} substituted with {}'.format(
+            obj.find('name').text, obj.find('weight').text, s_weight))
+        obj.find('weight').text = s_weight
+        weight = float(s_weight)
+    else:
+        weight = float(obj.find('weight').text)
+    # Correct type to RS supported naming
+    # RS Compatible types
+    obj_types = {'Max': 'MaxDose',
+                 'Min': 'MinDose',
+                 'UD': 'UniformDose',
+                 'MinEud': 'MinEud',
+                 'MaxEud': 'MaxEud',
+                 'TarEud': 'TargetEud',
+                 'DFO': 'DoseFallOff'}
+    if obj.find('type').text in obj_types:
+        function_type = obj_types[obj_types('type').text]
+    elif obj.find('type').text == 'DX':
+        if obj.find('type').attrib['dir'] == "ge" or obj.find('type').attrib['dir'] == "gt":
+            function_type = 'MinDvh'
+        elif obj.find('type').attrib['dir'] == "le" or obj.find('type').attrib['dir'] == "lt":
+            function_type = 'MaxDvh'
+    else:
+        logging.debug('add_objective: Unsupported function type for ROI: {} with type: {}'.format(
+            obj.find('text').name, obj.find('type').text))
+    #
+    # Add special types
+    if 'Eud' in obj.find('type').text:
+        eud_a = float(obj.find('type').attrib['a'])
+    if 'constaint' not in obj.find('type').attrib:
+        constraint = False
+    else:
+        if obj.find('type').attrib['constraint'] == 'True':
+            constraint = True
+        elif obj.find('type').attrib['constraint'] == 'False':
+            constraint = False
+        else:
+            logging.warning('add_objective: Unsupported constraint specification {}'.format(
+                obj.find('type').attrib['constraint']))
+    if 'adapt' in obj.find('type').attrib:
+        if obj.find('type').attrib['adapt'] == 'True':
+            adapt_dose = True
+        elif obj.find('type').attrib['adapt'] == 'False':
+            adapt_dose = False
+        else:
+            logging.warning('add_objective: Unsupported type for attribute {}'.format(
+                obj.find('type').attrib['adapt']))
+    if obj.find('type').name == 'DFO':
+        if obj.find('dose').attrib['units'] == "%":
+            high_dose = float(s_dose) * float(obj.find('dose').text) / 100
+            low_dose = float(s_dose) * float(obj.find('dose').attrib['low']) / 100
+        elif obj.find('dose').attrib['units'] == "Gy":
+            high_dose = float(obj.find('dose').text)
+            low_dose = float(obj.find('dose').attrib['low'])
+        else:
+            logging.warning('add_objective: Unsupported doses for Dose Fall Off')
+        if 'dist' in obj.find('type').attrib:
+            low_dose_dist = float(obj.find('type').attrib['dist'])
+        else:
+            logging.warning('add_objective: Unknown low dose distance for Dose Fall Off')
+
+    # UniformDose: Dose, Weight, % Volume=30?
+    # UniformityConstraint?
+    # Deal with robustness
 
     # Find current Beamset Number and determine plan optimization
     OptIndex = 0
@@ -79,30 +182,24 @@ def add_objective(plan,
             ))
 
     retval_0 = plan_optimization.AddOptimizationFunction(FunctionType=function_type,
-                                                         RoiName=roi_name,
-                                                         IsConstraint=is_constraint,
+                                                         RoiName=roi,
+                                                         IsConstraint=constraint,
                                                          RestrictAllBeamsIndividually=False,
                                                          RestrictToBeam=None,
                                                          IsRobust=robust,
                                                          RestrictToBeamSet=restrict_beamset,
                                                          UseRbeDose=False)
-
-    retval_0.DoseFunctionParameters.Weight=weight
-    if dose:
-        retval_0.DoseFunctionParameters.DoseLevel = dose
+    retval_0.DoseFunctionParameters.Weight = weight
+    retval_0.DoseFunctionParameters.DoseLevel = dose
     if volume:
         retval_0.PercentVolume = volume
-    if eud_a:
+    if 'Eud' in function_type:
         retval_0.DoseFunctionParameters.EudParameterA = eud_a
-    # It should be noted that the next 4 inputs should be combined into a "type" based argument for the
-    # dose fall off type of optimization option.
-    if high_dose:
+    # Dose fall off type of optimization option.
+    if function_type == 'DoseFallOff':
         retval_0.DoseFunctionParameters.HighDoseLevel = high_dose
-    if low_dose:
         retval_0.DoseFunctionParameters.LowDoseLevel = low_dose
-    if low_dose_dist:
         retval_0.DoseFunctionParameters.LowDoseDistance = low_dose_dist
-    if adapt_dose:
         retval_0.DoseFunctionParameters.AdaptToTargetDoseLevels = adapt_dose
 
 
@@ -123,6 +220,12 @@ def main():
     path_protocols = os.path.join(os.path.dirname(__file__), protocol_folder, institution_folder,file)
     tree = select_objectives(filename=path_protocols)
     logging.debug("selected file {}".format(path_protocols))
+    # TODO::
+    # Extend this for multiple objective sets found within a file
+    # Consider adding functionality for protocols, orders, etc...
+    # Parse each type in a separate function
+    # Add matching elements
+    # Add objective function should know whether something is absolute or relative for dose and volume
     if tree.getroot().tag == 'objectiveset':
         logging.debug("parsing xml: {}".format(file))
         n = tree.find('name').text
@@ -133,6 +236,14 @@ def main():
             o_name = o.find('name').text
             o_type = o.find('type').text
             logging.debug("objective: {} found with type {}".format(o_name, o_type))
+            add_objective(o,
+                          case=case,
+                          plan=plan,
+                          beamset=beamset,
+                          s_roi=None,
+                          s_dose=None,
+                          s_weight=None,
+                          restrict_beamset=None)
     else:
         logging.debug('Could not find objective set using tree = {}'.format(tree))
     #for o in objs.findall('objectiveset'):
@@ -142,24 +253,14 @@ def main():
     #       'constraint': False,
     #       'eud_a': 3,
     #       'dose': 50}
-    # Types are:
-    # MinDose: Dose, Weight
-    # MaxDose: Dose, Weight
-    # MinDvh: Dose, Weight, Volume
-    # MaxDvh: Dose, Weight, Volume
-    # UniformDose: Dose, Weight, % Volume=30?
-    # MinEud: Dose, Weight, ParamA
-    # MaxEud: Dose, Weight, ParamA
-    # TargetEud: Dose, Weight, ParamA
-    # DoseFallOff: HighDose, LowDose, LowDoseDistance, AdaptToTarget, ?RelativeLowDoseWeight=1
-    # UniformityConstraint?
-    add_objective(plan=plan,
-                  beamset=beamset,
-                  function_type='MinEud',
-                  weight=10,
-                  roi_name='PTV1',
-                  eud_a=3,
-                  dose=5000)
+
+    #add_objective(plan=plan,
+    #              beamset=beamset,
+    #              function_type='MinEud',
+    #              weight=10,
+    #              roi_name='PTV1',
+    #              eud_a=3,
+    #              dose=5000)
 
 
 if __name__ == '__main__':
