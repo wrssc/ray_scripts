140f34bc208cbb8fcce8771f1c70e03185fbeb50
Adam Bayliss <rabayliss@wisc.edu>
2019-05-02 12:40:35 -0500

    Update automated_plan_optimization.py

--- general/automated_plan_optimization.py
+++ general/automated_plan_optimization.py
@@ -214,6 +214,8 @@ def check_min_jaws(plan_opt, min_dim):
                     for m in s.LeafPositions[1]:
                         if m > max_mlc_bank_1:
                             max_mlc_bank_1 = m
+                    logging.debug('Segment: {}, MaxBank0:{}, MaxBank1:{},'.format(s.SegmentNumber,
+                                                                                  max_mlc_bank_0, max_mlc_bank_1))
                     # Searching from the lowest MLC number find the first open MLC
                     for i in range(0, n_mlc-1):
                         if s.LeafPositions[0][i] > 0 or s.LeafPositions[1][i] > 0:
@@ -222,8 +224,6 @@ def check_min_jaws(plan_opt, min_dim):
                     for i in range(n_mlc-1,0,-1):
                         if s.LeafPositions[0][i] > 0 or s.LeafPositions[1][i] > 0:
                             max_open_negY = i
-                    logging.debug('Segment: {}, MaxBank0:{}, MaxBank1:{},'.format(s.SegmentNumber,
-                                                                                  max_mlc_bank_0, max_mlc_bank_1))
                     logging.debug('Segment: {}, MaxYpos:{}, MaxYneg:{},'.format(s.SegmentNumber,
                                 max_open_posY, max_open_negY))
 
