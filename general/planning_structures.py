6539f3128acf1cc0d68b66bb6ed813c2221272bf
Adam Bayliss <rabayliss@wisc.edu>
2019-05-15 15:00:39 -0500

    Update planning_structures.py

--- general/planning_structures.py
+++ general/planning_structures.py
@@ -482,11 +482,10 @@ def main():
     source_doses = []
     input_source_list = [None] * n
     source_doses = [None] * n
-    sorted_initial_dialog = sorted(initial_dialog.values, key=lambda k: int(k.split('_')[0]))
-    for k, v in sorted_initial_dialog:#initial_dialog.values.iteritems():
+    for k, v in initial_dialog.values.iteritems():
         logging.debug("Dialog entries for k and v: {} and {}".format(k,v))
         # Grab the first two characters in the key and convert to an index
-        i_char = k[0:1]
+        i_char = k[:2]
         logging.debug('key is {}'.format(i_char))
         indx = int(i_char)-1
         logging.debug('Index for key is {}'.format(indx))
