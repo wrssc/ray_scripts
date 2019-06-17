5cd9a8f0d34ee9d07e158b4437c27b6bf3079176
Adam Bayliss <rabayliss@wisc.edu>
2019-06-17 15:31:03 -0500

    Update Objectives.py

--- library/Objectives.py
+++ library/Objectives.py
@@ -12,7 +12,37 @@ import xml.etree.ElementTree
 import connect
 
 
-def select_objectives(folder=None,filename=None):
+def find_optimization_index(plan, beamset):
+    """
+
+    :param case: current case
+    :param plan: current plan
+    :param beamset: current beamset
+    :return: OptIndex: index of the optimization used by the current beamset
+    """
+    # Find current BeamSet Number and determine plan optimization
+    indices = []
+    for OptIndex, opts in enumerate(plan.PlanOptimizations):
+        try:
+            opts.OptimizedBeamSets[beamset.DicomPlanLabel]
+            indices.append(OptIndex)
+        except:
+            pass
+    # Ensure we have a unique match or exit
+    if len(indices) == 1:
+        # Found our index.  We will use a shorthand for the remainder of the code
+        OptIndex = indices[0]
+        plan_optimization = plan.PlanOptimizations[OptIndex]
+    elif len(indices) == 0:
+        logging.warning("Beamset optimization for {} could not be found.".format(beamset.DicomPlanLabel))
+        sys.exit("Could not find beamset optimization")
+    elif len(indices) > 1:
+        logging.warning("Beamset has multiple optimizations, cannot proceed")
+        sys.exit("Multiple beamset optimizations found in current plan.Cannot proceed")
+    return OptIndex
+
+
+def select_objectives(folder=None, filename=None):
     """
 
     :param filename: os joined protocol name
@@ -25,112 +55,206 @@ def select_objectives(folder=None,filename=None):
         # Search protocol list, parsing each XML file for protocols and goalsets
         logging.debug('Searching folder {} for protocols, goal sets'.format(folder))
         for f in os.listdir(folder):
+            # This guy should prompt the user to find the appropriate file
             if f.endswith('.xml'):
                 tree = xml.etree.ElementTree.parse(os.path.join(folder, f))
     return tree
 
 
-def add_objective(plan,
-                  beamset,
-                  function_type,
-                  roi_name,
-                  weight=None,
-                  robust=False,
-                  is_constraint=False,
-                  dose=None,
-                  volume=None,
-                  eud_a = None,
-                  restrict_beamset=None,
-                  low_dose_dist=None,
-                  high_dose=None,
-                  low_dose=None,
-                  adapt_dose=False,
-                  ):
+def add_objective(obj, case, plan, beamset,
+                  s_roi=None, s_dose=None,
+                  s_weight=None, restrict_beamset=None):
     """
-    Add objective to RayStation
-    :return:
+    adds an objective function to the optimization in RayStation after
+    :param obj: child (roi-tag) of an ElementTree - consider mak
+    :param case: RS case
+    :param plan: RS plan
+    :param beamset: RS beamset
+    :param s_roi: substitute roi for the name tag
+    :param s_dose: substitute dose for the dose tag, str dose in Gy, this will be used as
+                    reference dose in Dose Fall Off operations
+    :param s_weight: substitute weight from protocol-defined tag, str weight
+    :param restrict_beamset: if co-optimization is used, this is needed to restrict an objective to a
+                            given beamset
+    :return: (none) although obj is directly modified by this function
     """
-
-    # Find current Beamset Number and determine plan optimization
-    OptIndex = 0
-    IndexNotFound = True
-    # In RS, OptimizedBeamSets objects are keyed using the DicomPlanLabel, or Beam Set name.
-    # Because the key to the OptimizedBeamSets presupposes the user knows the PlanOptimizations index
-    # this while loop looks for the PlanOptimizations index needed below by searching for a key
-    # that matches the BeamSet DicomPlanLabel
-    # This can likely be replaced with a list comprehension
-    while IndexNotFound:
-        try:
-            OptName = plan.PlanOptimizations[OptIndex].OptimizedBeamSets[beamset.DicomPlanLabel].DicomPlanLabel
-            IndexNotFound = False
-        except Exception:
-            IndexNotFound = True
-            OptIndex += 1
-    if IndexNotFound:
-        logging.warning("optimize_plan: Beamset optimization for {} could not be found.".format(beamset.DicomPlanLabel))
-        sys.exit("Could not find beamset optimization")
+    # Parse the objectives
+    #
+    # If an roi was specified replace the name tag
+    if s_roi:
+        logging.debug("Objective is added for protocol ROI: {} using plan ROI: {}".format(
+            obj.find('name').text, s_roi))
+        roi = s_roi
+        obj.find('name').text = roi
     else:
-        # Found our index.  We will use a shorthand for the remainder of the code
-        plan_optimization = plan.PlanOptimizations[OptIndex]
-        #plan_optimization_parameters = plan.PlanOptimizations[OptIndex].OptimizationParameters
-        logging.info(
-            'optimize_plan: Optimization found, proceeding with plan.PlanOptimization[{}] for beamset {}'.format(
-                OptIndex, plan_optimization.OptimizedBeamSets[beamset.DicomPlanLabel].DicomPlanLabel
-            ))
+        roi = obj.find('name').text
+    #
+    # Deal with relative or absolute volumes, modify the volume tag
+    # (RayStation only allows relative volume roi's
+    # :TODO: Check how to find existence of a tag in elementtree
+    if obj.find('volume') is None:
+        volume = None
+    else:
+        # If the user specified an absolute volume convert this into %
+        if obj.find('volume').attrib["units"] == "cc":
+            try:
+                t = case.PatientModel.StructureSets[exam.Name]. \
+                    RoiGeometries[s_roi]
+                if t.HasContours():
+                    roi_vol = t.GetRoiVolume()
+                    volume = int(float(obj.find('volume').text) / roi_vol)
+                    logging.debug('ROI: {} Protocol volume {} substituted with {}'.format(
+                        obj.find('name').text, obj.find('volume').text, volume))
+                    obj.find('volume').text = str(volume)
+                    obj.find('volume').attrib["units"] = "%"
+                else:
+                    logging.warning('add_objective: {} has no contours, index undefined'.format(s_roi))
+            except:
+                logging.warning('add_objective: Error getting volume for {}, volume => 0.0'.format(s_roi))
+        elif obj.find('volume').attrib["units"] == "%":
+            volume = int(obj.find('volume').text)
+    # Modify the dose tag if relative
+    if s_dose:
+        logging.debug('add_objective: ROI: {} Protocol dose {} {} substituted with {} Gy'.format(
+            obj.find('name').text, obj.find('dose').text, obj.find('dose').attrib["units"], s_dose))
+        if obj.find('dose').attrib["units"] == "%":
+            obj.find('dose').attrib["units"] = "Gy"
+            # Change the element to the substitute dose times the percentage of the reference
+            obj.find('dose').text = float(s_dose) * float(obj.find('dose').text) / 100
+            dose = float(obj.find('dose').text) * 100
+        else:
+            obj.find('dose').text = float(s_dose) * 100
+    else:
+        dose = float(obj.find('dose').text) * 100
+    #
+    # Read the weight variable
+    if s_weight:
+        logging.debug('add_objective: ROI: {} Protocol weight {} substituted with {}'.format(
+            obj.find('name').text, obj.find('weight').text, s_weight))
+        obj.find('weight').text = s_weight
+        weight = float(s_weight)
+    else:
+        weight = float(obj.find('weight').text)
+    # Correct type to RS supported naming
+    # If the objective does not require directional evaluation (like those in obj_types)
+    # we can reassign directly. Otherwise, we need to parse direction.
+    obj_types = {'Max': 'MaxDose',
+                 'Min': 'MinDose',
+                 'UD': 'UniformDose',
+                 'MinEud': 'MinEud',
+                 'MaxEud': 'MaxEud',
+                 'TarEud': 'TargetEud',
+                 'DFO': 'DoseFallOff'}
+    if obj.find('type').text in obj_types:
+        function_type = obj_types[obj.find('type').text]
+    elif obj.find('type').text == 'DX':
+        if obj.find('type').attrib['dir'] == "ge" or obj.find('type').attrib['dir'] == "gt":
+            function_type = 'MinDvh'
+        elif obj.find('type').attrib['dir'] == "le" or obj.find('type').attrib['dir'] == "lt":
+            function_type = 'MaxDvh'
+    else:
+        logging.debug('add_objective: Unsupported function type for ROI: {} with type: {}'.format(
+            obj.find('name').text, obj.find('type').text))
+    #
+    # Add special types
+    if 'Eud' in obj.find('type').text:
+        eud_a = float(obj.find('type').attrib['a'])
+    if 'constraint' not in obj.find('type').attrib:
+        constraint = False
+    else:
+        if obj.find('type').attrib['constraint'] == 'True':
+            constraint = True
+        elif obj.find('type').attrib['constraint'] == 'False':
+            constraint = False
+        else:
+            logging.warning('add_objective: Unsupported constraint specification {}'.format(
+                obj.find('type').attrib['constraint']))
+    if 'adapt' in obj.find('type').attrib:
+        if obj.find('type').attrib['adapt'] == 'True':
+            adapt_dose = True
+        elif obj.find('type').attrib['adapt'] == 'False':
+            adapt_dose = False
+        else:
+            logging.warning('add_objective: Unsupported type for attribute {}'.format(
+                obj.find('type').attrib['adapt']))
+    if obj.find('type').text == 'DFO':
+        if obj.find('dose').attrib['units'] == "%":
+            high_dose = 100 * float(s_dose) * float(obj.find('dose').text) / 100
+            low_dose = 100 * float(s_dose) * float(obj.find('dose').attrib['low']) / 100
+        elif obj.find('dose').attrib['units'] == "Gy":
+            high_dose = 100 * float(obj.find('dose').text)
+            low_dose = 100 * float(obj.find('dose').attrib['low'])
+        else:
+            logging.warning('add_objective: Unsupported doses for Dose Fall Off')
+        if 'dist' in obj.find('type').attrib:
+            low_dose_dist = float(obj.find('type').attrib['dist'])
+        else:
+            logging.warning('add_objective: Unknown low dose distance for Dose Fall Off')
+        logging.debug('DFO object found.  High Dose: {}, Low Dose: {}, Distance: {}'.format(
+            high_dose, low_dose, low_dose_dist))
+    if 'robust' in obj.find('type').attrib:
+        if obj.find('type').attrib['robust'] == 'False':
+            robust = False
+        elif obj.find('type').attrib['robust'] == 'True':
+            robust = True
+        else:
+            logging.warning('Unsupported robustness type {}'.format(obj.find('type').attrib['robust']))
+    else:
+        robust = False
 
+    # UniformDose: Dose, Weight, % Volume=30?
+    # UniformityConstraint?
 
-    retval_0 = plan_optimization.AddOptimizationFunction(FunctionType=function_type,
-                                                  RoiName=roi_name,
-                                                  IsConstraint=is_constraint,
-                                                  RestrictAllBeamsIndividually=False,
-                                                  RestrictToBeam=None,
-                                                  IsRobust=robust,
-                                                  RestrictToBeamSet=restrict_beamset,
-                                                  UseRbeDose=False)
+    OptIndex = find_optimization_index(plan=plan,beamset=beamset)
+    plan_optimization = plan.PlanOptimizations[OptIndex]
+    # Find current BeamSet Number and determine plan optimization
+    # indices = []
+    # for OptIndex, opts in enumerate(plan.PlanOptimizations):
+    #     try:
+    #         opts.OptimizedBeamSets[beamset.DicomPlanLabel]
+    #         indices.append(OptIndex)
+    #     except:
+    #         pass
+    # # Ensure we have a unique match or exit
+    # if len(indices) == 1:
+    #     # Found our index.  We will use a shorthand for the remainder of the code
+    #     OptIndex = indices[0]
+    #     plan_optimization = plan.PlanOptimizations[OptIndex]
+    # elif len(indices) == 0:
+    #     logging.warning("Beamset optimization for {} could not be found.".format(beamset.DicomPlanLabel))
+    #     sys.exit("Could not find beamset optimization")
+    # elif len(indices) > 1:
+    #     logging.warning("Beamset has multiple optimizations, cannot proceed")
+    #     sys.exit("Multiple beamset optimizations found in current plan.Cannot proceed")
 
-    if dose:
-        retval_0.DoseFunctionParameters.DoseLevel = dose
-    if volume:
-        retval_0.PercentVolume = volume
-    if eud_a:
-        retval_0.EudParameterA = eud_a
-    if weight:
-        retval_0.DoseFunctionParameters.Weight = weight
-    # It should be noted that the next 4 inputs should be combined into a "type" based argument for the
-    # dose fall off type of optimization option.
-    if high_dose:
-        retval_0.DoseFunctionParameters.HighDoseLevel = high_dose
-    if low_dose:
-        retval_0.DoseFunctionParameters.LowDoseLeve = low_dose
-    if low_dose_dist:
-        retval_0.DoseFunctionParameters.LowDoseDistance = low_dose_dist
-    if adapt_dose:
-        retval_0.DoseFunctionParameters.AdaptToTargetDoseLevels = adapt_dose
-
-
-def main():
-    """ Temp chunk of code to try to open an xml file"""
+    # Add the objective
     try:
-        patient = connect.get_current('Patient')
-        case = connect.get_current("Case")
-        examination = connect.get_current("Examination")
-        plan = connect.get_current("Plan")
-        beamset = connect.get_current("BeamSet")
+        o = plan_optimization.AddOptimizationFunction(FunctionType=function_type,
+                                                      RoiName=roi,
+                                                      IsConstraint=constraint,
+                                                      RestrictAllBeamsIndividually=False,
+                                                      RestrictToBeam=None,
+                                                      IsRobust=robust,
+                                                      RestrictToBeamSet=restrict_beamset,
+                                                      UseRbeDose=False)
+        o.DoseFunctionParameters.Weight = weight
+        if volume:
+            o.DoseFunctionParameters.PercentVolume = volume
+        if 'Eud' in function_type:
+            o.DoseFunctionParameters.EudParameterA = eud_a
+            # Dose fall off type of optimization option.
+        if function_type == 'DoseFallOff':
+            o.DoseFunctionParameters.HighDoseLevel = high_dose
+            o.DoseFunctionParameters.LowDoseLevel = low_dose
+            o.DoseFunctionParameters.LowDoseDistance = low_dose_dist
+            o.DoseFunctionParameters.AdaptToTargetDoseLevels = adapt_dose
+            # For all types other than DoseFallOff, the dose is simply entered here
+        else:
+            o.DoseFunctionParameters.DoseLevel = dose
+        logging.debug("Added objective for ROI: " +
+                      "{}, type {}, dose {}, weight {}, for beamset {} with restriction: {}".format(
+                          roi, function_type, dose, weight, beamset.DicomPlanLabel, restrict_beamset))
     except:
-        logging.warning("patient, case and examination must be loaded")
-
-    file = 'planning_structs_conventional.xml'
-    #obj = {'function_type': 'MinEud',
-    #       'roi_name': 'PTV1',
-    #       'constraint': False,
-    #       'eud_a': 3,
-    #       'dose': 50}
-    add_objective(plan=plan,
-                  function_type='MinEud',
-                  roi_name='PTV1',
-                  eud_a=3,
-                  dose=50)
-
-
-if __name__ == '__main__':
-    main()
+        logging.debug("Failed to add objective for ROI:" +
+                      " {}, type {}, dose {}, weight {}, for beamset {}".format(
+                          roi, function_type, dose, weight, restrict_beamset))
