65dbbfe9542adda9c777f0d53640127a1901dd10
Adam Bayliss <rabayliss@wisc.edu>
2019-06-17 16:58:36 -0500

    Moving protocol selection into library
    Moved the select_objective_protocol function into Objectives.py
    -Added minimal support for a filename based call but have not tested.

--- general/create_objectives.py
+++ general/create_objectives.py
@@ -14,56 +14,6 @@ import Objectives
 import UserInterface
 
 
-def select_objective_protocol():
-    """
-    Prompt user to select the objective xml file they want to use
-    :return: tree: Elementtree with user-selected objectives loaded
-    """
-    protocol_folder = r'../protocols'
-    institution_folder = r'UW'
-    # os join autoresolves the path
-    path_objectives = os.path.join(os.path.dirname(__file__),
-                                   protocol_folder,
-                                   institution_folder)
-
-    # Review specified directory for any objectiveset tags
-    logging.debug('Searching folder {} for objectivesets'.format(path_objectives))
-    objective_sets = {}
-    for f in os.listdir(path_objectives):
-        if f.endswith('.xml'):
-            tree = xml.etree.ElementTree.parse(os.path.join(path_objectives, f))
-            if tree.getroot().tag == 'objectiveset':
-                n = tree.find('name').text
-                logging.debug('Found objectiveset {} in {}'.format(n, f))
-                # Debugging fun
-                if n in objective_sets:
-                    # objective_sets[n].extend(tree.getroot())
-                    logging.debug("Objective set {} already in list".format(n))
-                else:
-                    objective_sets[n] = tree
-    # Augment the list to include all xml files found with an "objectiveset" tag in name
-    input_dialog = UserInterface.InputDialog(
-        inputs={'i': 'Select Objective Set'},
-        title='Objective Selection',
-        datatype={'i': 'combo'},
-        initial={},
-        options={'i': list(objective_sets.keys())},
-        required=['i'])
-    response = input_dialog.show()
-    # Close on cancel
-    if response == {}:
-        logging.info('create_objective cancelled by user')
-   ##     status.finish('User cancelled create objective creation.')
-        sys.exit('create_objective cancelled by user')
-    for k in input_dialog.values:
-        logging.debug('This is key {} value {}'.format(k,input_dialog.values[k]))
-
-    # logging.debug('user selected {}').format(input_dialog.values['i'])
-    tree = objective_sets[input_dialog.values['i']]
-    # Eventually move this selection dialog to Objectives.py so we can clean this up
-    # tree = Objectives.select_objectives(input_dialog.values['i'])
-    return tree
-
 
 def main():
     """ Temp chunk of code to try to open an xml file"""
@@ -81,7 +31,7 @@ def main():
     file = 'planning_structs_conventional.xml'
     path_protocols = os.path.join(os.path.dirname(__file__), protocol_folder, institution_folder, file)
     tree = Objectives.select_objectives(filename=path_protocols)
-    tree = select_objective_protocol()
+    tree = Objectives.select_objective_protocol()
     logging.debug("selected file {}".format(path_protocols))
     # TODO::
     # Extend this for multiple objective sets found within a file
--- library/Objectives.py
+++ library/Objectives.py
@@ -42,6 +42,68 @@ def find_optimization_index(plan, beamset):
     return OptIndex
 
 
+def select_objective_protocol(folder=None, filename=None):
+    """
+    :param filename: os joined protocol name
+    :param folder: folder from os to search within
+    :return: tree: elementtree from xml file
+
+    Prompt user to select the objective xml file they want to use
+    :return: tree: Elementtree with user-selected objectives loaded
+    """
+    if filename:
+        tree = xml.etree.ElementTree.parse(filename)
+        return tree
+    elif folder:
+        path_objectives = os.path.join(os.path.dirname(__file__),
+                                       protocol_folder,
+                                       institution_folder)
+    else:
+        protocol_folder = r'../protocols'
+        institution_folder = r'UW'
+        # os join autoresolves the path
+        path_objectives = os.path.join(os.path.dirname(__file__),
+                                       protocol_folder,
+                                       institution_folder)
+
+    # Review specified directory for any objectiveset tags
+    logging.debug('Searching folder {} for objectivesets'.format(path_objectives))
+    objective_sets = {}
+    for f in os.listdir(path_objectives):
+        if f.endswith('.xml'):
+            tree = xml.etree.ElementTree.parse(os.path.join(path_objectives, f))
+            if tree.getroot().tag == 'objectiveset':
+                n = tree.find('name').text
+                logging.debug('Found objectiveset {} in {}'.format(n, f))
+                # Debugging fun
+                if n in objective_sets:
+                    # objective_sets[n].extend(tree.getroot())
+                    logging.debug("Objective set {} already in list".format(n))
+                else:
+                    objective_sets[n] = tree
+    # Augment the list to include all xml files found with an "objectiveset" tag in name
+    input_dialog = UserInterface.InputDialog(
+        inputs={'i': 'Select Objective Set'},
+        title='Objective Selection',
+        datatype={'i': 'combo'},
+        initial={},
+        options={'i': list(objective_sets.keys())},
+        required=['i'])
+    response = input_dialog.show()
+    # Close on cancel
+    if response == {}:
+        logging.info('create_objective cancelled by user')
+   ##     status.finish('User cancelled create objective creation.')
+        sys.exit('create_objective cancelled by user')
+    for k in input_dialog.values:
+        logging.debug('This is key {} value {}'.format(k,input_dialog.values[k]))
+
+    # logging.debug('user selected {}').format(input_dialog.values['i'])
+    tree = objective_sets[input_dialog.values['i']]
+    # Eventually move this selection dialog to Objectives.py so we can clean this up
+    # tree = Objectives.select_objectives(input_dialog.values['i'])
+    return tree
+
 def select_objectives(folder=None, filename=None):
     """
 
