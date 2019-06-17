09f63e7b5b5786de935bf1a2435e97f0918973af
Adam Bayliss <rabayliss@wisc.edu>
2019-06-17 14:40:29 -0500

    Update create_objectives.py

--- general/create_objectives.py
+++ general/create_objectives.py
@@ -60,7 +60,8 @@ def select_objective_protocol():
         logging.debug('This is key {} value {}'.format(k,input_dialog.values[k]))
 
     # logging.debug('user selected {}').format(input_dialog.values['i'])
-    tree = Objectives.select_objectives(input_dialog.values['i'])
+    tree = objective_sets[input_dialog.values['i']]
+    # tree = Objectives.select_objectives(input_dialog.values['i'])
     return tree
 
 
