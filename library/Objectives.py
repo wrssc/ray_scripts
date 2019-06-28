a172c00cb9dab6098d9b0d76c804fc8aff7d86a1
Adam Bayliss <rabayliss@wisc.edu>
2019-06-27 20:16:56 -0500

    Update Objectives.py

--- library/Objectives.py
+++ library/Objectives.py
@@ -91,6 +91,9 @@ def select_objective_protocol(folder=None, filename=None):
                 # Find the objectivesets:
                 # These get loaded for protocols regardless of orders
                 protocol_obj_set = protocol.findall('./objectiveset')
+                logging.debug("Found objective set {} in {}".format(
+                    protocol_obj_set.get("name"), protocol.get("name")
+                ))
 
                 for pos in protocol_obj_set:
                     protocol_objective_set = [protocol_objective_set, pos]
