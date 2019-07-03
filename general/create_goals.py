b1c53a1d557c035a9d966a74b2bfd21ff514303d
Adam Bayliss <rabayliss@wisc.edu>
2019-07-03 11:18:04 -0500

    Update create_goals.py

--- general/create_goals.py
+++ general/create_goals.py
@@ -615,8 +615,8 @@ def main():
                     s_dose = float(translation_map[o_r][1]) * float(o_d) / 100
                     if o_t == 'DFO':
                         o.find('dose').attrib['units'] = "Gy"
-                        s_dose = s_dose / 100
                         o.find('dose').attrib['low'] = s_dose * float(o.find('dose').attrib['low'])
+                        s_dose = s_dose / 100
                     Objectives.add_objective(o,
                                              exam=exam,
                                              case=case,
