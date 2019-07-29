09084164651f6d87135e9633a6e1a75232e5236c
Adam Bayliss <rabayliss@wisc.edu>
2019-07-29 15:40:46 -0500

    Update Tomo DQA.py

--- qa_preparation/Tomo DQA.py
+++ qa_preparation/Tomo DQA.py
@@ -67,6 +67,9 @@ def main():
     # find the correct verification plan
     index_not_found = True
     logging.debug('Finding verification plan for {}'.format(beamset.DicomPlanLabel))
+    logging.debug('Verification plan[{}] is {}, not a match for {}.'.format(
+        0, plan.VerificationPlans[0].ForTreatmentPlan.Name, beamset.DicomPlanLabel
+    ))
     # Find the correct verification plan for this beamset
     try:
         indx = 0
