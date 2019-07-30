4a4447a272a7fe46769abd51eaed84a9409ae8ad
Adam Bayliss <rabayliss@wisc.edu>
2019-07-30 09:39:21 -0500

    Update DicomExport.py

--- library/DicomExport.py
+++ library/DicomExport.py
@@ -291,7 +291,6 @@ def send(case,
             logging.debug('Executing ScriptableDicomExport() to path {}'.format(original))
 
             if qa_plan is not None and filters is not None and 'tomo_dqa' in filters:
-                # It is not clear what is needed to identify the phantom as the Plan identifier
                 args = {'IgnorePreConditionWarnings': ignore_warnings,
                         'QaPlanIdentity': 'Patient',
                         'ExportFolderPath': original,
@@ -467,7 +466,7 @@ def send(case,
                     # If adding gantry period to TomoTherapy QA Plans
                     if gantry_period is not None:
                         # format and set tag to change
-                        t1 = pydicom.Tag('300d1040')
+                        t1 = pydicom.tag.Tag('300d1040')
 
                         # Add some white-space to the end of gantry period
                         str_gantry_period = gantry_period + ' '
