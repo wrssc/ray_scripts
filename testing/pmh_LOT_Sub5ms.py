37d8b8e1ce6d0d2eed80f92134586fc73bf32c18
Patrick Hill <phill353@users.noreply.github.com>
2020-01-13 15:41:31 -0600

    Update pmh_LOT_Sub5ms.py

--- testing/pmh_LOT_Sub5ms.py
+++ testing/pmh_LOT_Sub5ms.py
@@ -2,10 +2,16 @@
 
 Subtracts leaf time based on a uniformly distributed probability between -5 and 0 ms
 
+Leaves less than 60 ms after sampled perturbation are set to minimum LOT of 60 ms
+
 """
 
 import connect
 import random
+import logging
+
+logging.info('executing sinogram perterbation')
+logging.info('reducing non-zero leaves by uniformly sampled distribution from -5 to 0 ms')
 
 # get current patient, beam set, and beam
 patient = connect.get_current('Patient')
@@ -23,26 +29,27 @@ nseg = 0
 # loop through segments
 for seg in beam.Segments:
 
+    logging.info('updating segment {:n}'.format(nseg))
+
     # get current leaf open time array
     lots = seg.LeafOpenFraction
 
     # loop though array elements
     for ii in range(0,len(lots)):
 
-        # only change non-zero leaves
-        if lots[ii] != 0:
+        # only change non-zero and non-full LOT leaves
+        if lots[ii] != 0 and lots[ii] < 1:
+
+            # get old LOT
+            oldlot = lots[ii]
 
             # calculate new LOT value with random offset from -5 to 0 ms
-            newlot = lots[ii] + random.uniform(-offsetfrac,0)
+            newlot = oldlot + random.uniform(-offsetfrac,0)
 
             # add to LOT array, minimium at 60 ms and capping at 1.0
-            if newlot <= 1 and newlot >= minfrac:
+            if newlot >= minfrac:
 
                 lots[ii] = newlot
-
-            elif newlot > 1:
-
-                lots[ii] = 1.0
 
             else:
 
@@ -55,9 +62,10 @@ for seg in beam.Segments:
     nseg = nseg + 1
 
 # reset last segment to all zeros, this is required by RayStation to calculate
+logging.info('setting final segment to zero LOTs per RayStation requirements')
 for ii in range(0,len(lots)):
 
     beam.Segments[nseg-1].LeafOpenFraction[ii] = 0
 
 
-
+logging.info('sinogram perterbation complete')
