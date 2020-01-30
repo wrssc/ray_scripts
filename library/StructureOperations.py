8ee4f4480160b93b6aa550038838cfc2baa13998
Adam Bayliss <rabayliss@wisc.edu>
2020-01-30 15:08:05 -0600

    Update StructureOperations.py

--- library/StructureOperations.py
+++ library/StructureOperations.py
@@ -693,11 +693,12 @@ def match_roi(case, exam, plan, beamset, plan_rois, protocol_rois):
 
         m_logs = r'Q:\\RadOnc\RayStation\RayScripts\dev_logs'
         with open(os.path.normpath('{}/Matched_Structures.txt').format(m_logs), 'a') as match_file:
-            match_file.write('PlanName: {pn}, '.format(pn=beamset.DicomPlanLabel))
+            match_file.write('PlanName: {pn} :: '.format(pn=beamset.DicomPlanLabel))
         # for r in rois:
         #    if r == matches[r]:
         #        correct += 1
         with open(os.path.normpath('{}/Matched_Structures.txt').format(m_logs), 'a') as match_file:
             for k, v in response.iteritems():
                 match_file.write('{v}: {k}, '.format(k=k, v=v))
+            match_file.write('\n '.format(k=k, v=v))
         logging.debug('Correct matches on test set {} / {}'.format(correct, len(plan_rois)))
