51b995bca9e54c0275e5433c3a9c95a975e050dd
Adam Bayliss <rabayliss@wisc.edu>
2020-02-16 14:08:02 -0600

    Update BeamOperations.py

--- library/BeamOperations.py
+++ library/BeamOperations.py
@@ -1337,12 +1337,10 @@ class mlc_properties:
                         x1_diff = [x1_diff_0, x1_diff_1]
                         x2_diff = [x2_diff_0, x2_diff_1]
                     if cp == 0 or cp == 1:
-                        logging.debug('Beam {}: CP {}: Leaf {}:: MLC1 {}, MLC2 {} '.format(
-                            self.beam.Name,cp,l,self.banks[l,0,cp], self.banks[l,1,cp]
-                        ))
-                        logging.debug('CP {}, Leaf {}: x1diff {} :: x2diff {}'.format(cp, l, x1_diff, x2_diff))
-                        logging.debug('CP {}, Leaf0 {}: {}'.format(cp, l, closed_leaf_gaps[1, 0, cp]))
-                        logging.debug('CP {}, Leaf1 {}: {}'.format(cp, l, closed_leaf_gaps[1, 1, cp]))
+                        logging.debug('Beam {}: CP {}: Leaf {}:: MLC1 {}, MLC2 {}, x1diff {}, x2diff {}, gap {} '
+                            .format(
+                            self.beam.Name, cp, l, self.banks[l,0,cp], self.banks[l,1,cp], x1_diff,x2_diff,
+                            closed_leaf_gaps[l,0,cp]))
                     if np.all(x1_diff <= threshold) and np.all(x2_diff <= threshold) and not ignore_leaf_pair:
                         closed_leaf_gaps[l, :, cp] = True
                     else:
