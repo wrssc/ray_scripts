1838aeefabbe8304a002a917181f0bbe0fd8d78b
Adam Bayliss <rabayliss@wisc.edu>
2020-02-16 14:55:19 -0600

    Update BeamOperations.py

--- library/BeamOperations.py
+++ library/BeamOperations.py
@@ -1319,14 +1319,17 @@ class mlc_properties:
                         ignore_leaf_pair = True
                     else:
                         ignore_leaf_pair = False
+                    #
                     # Check if the leaf gap is a "closed leaf gap" that is not moving in adjacent control points
-                    # First control point only
                     # See if leaf pair moves from one iteration to the next
+                    #
+                    # First control point only
                     if cp == 0:
                         x1_diff_0 = np.abs(self.banks[l, 0, cp + 1] - self.banks[l, 0, cp])
                         x2_diff_0 = np.abs(self.banks[l, 1, cp + 1] - self.banks[l, 1, cp])
                         x1_diff_1 = np.abs(self.banks[l, 0, cp + 2] - self.banks[l, 0, cp + 1])
                         x2_diff_1 = np.abs(self.banks[l, 1, cp + 2] - self.banks[l, 1, cp + 1])
+                    # Last control point only
                     elif cp == number_cp - 1:
                         x1_diff_0 = np.abs(self.banks[l, 0, cp - 1] - self.banks[l, 0, cp])
                         x2_diff_0 = np.abs(self.banks[l, 1, cp - 1] - self.banks[l, 1, cp])
