ee59f5bcec760bfab275d258beb55ea658f30259
Adam Bayliss <rabayliss@wisc.edu>
2020-06-10 14:46:27 -0500

    (StructureOperations: planning_structure_preferences, AutoLoadPlan.py):
    StructureOperations: planning_structure_preferences
    Build: added properties to store underdose and uniform dose structures and standoffs
    Build: added optional argument to exists_roi that returns only existing rois sent to the function
    Build: Add a Levenshtein match of aliases to search. -Debug
    
    AutoLoadPlan.py
    Build: Added a test of input commands for planning structures. It will make sure all appropriate elements are present in the xml input and parse the input data from the csv file
    Build: Moved planning structure generation to its own function, removed testing elements from it
    Build: Added optimize plan to automation.
    * Imported optimize function
    * Convert xml to dict to dataframe
    * populate the kwargs for optimize plan
    * embed optimization in a try/except
    Build: Added setting of CT-Density if needed - patient save needed, along with update structures
    Build: Added testing of targets to make sure they are present in the case
    Build: Added a stub for testing optimization inputs. To be expanded
    fix: unresolved reference to OptimizationOperations
    fix: incorrect xml to dict on optimizatio
    fix: bug in dataframe call
    firx: close script status upon report
    Clean: Output header
    Clean: Code clea:nup main
    
    OptimizationOperations:
    build: created an xml to dict method for optimization configuration
    build: added an optional "close" to automatically shut down status upon finish
    fix: optimization does not seem to define the dose grid for a new plan. Explicitly doing this.
    fix: added new SBRT names for lung to the list of names searched for "Treat Settings"
    UWHeadNeck.xml
    Build: added optimization_configuration
    Build: added check on CT imaging system defined. Required examination as parameter
    Fix: Missing tag
    Fix: Added a check to see if structures should be created (no workflow specified in input file
    
    TG-263.xml
    -Added aliases for Globes
    TODO: Delete the planning structure definitions from main.
    TODO: Move all element populations to dataframes
    TODO: Test with Tomo, SMLC, and VMAT

--- library/StructureOperations.py
+++ library/StructureOperations.py
@@ -1000,7 +1000,6 @@ def find_normal_structures_match(rois, standard_rois, num_matches=None):
         unsorted_matches = []
         # Create an ordered list. If there is an exact match,
         # make the first element in the ordered list that.
-        re_ci_r = re.compile(r, re.IGNORECASE)
         for a_key, a_val in aliases.items():
             [alias_match, alias_dist] = levenshtein_match(r, a_val, num_matches)
             if any(ad < len(r)*0.5 * match_threshold for ad in alias_dist):
@@ -1011,9 +1010,11 @@ def find_normal_structures_match(rois, standard_rois, num_matches=None):
                     lr_mismatch = True
                 if not lr_mismatch:
                     unsorted_matches.append((alias_distance,a_key))
-            ##  for v in a_val:
-            ##      if re.match(re_ci_r, v):
-            ##      unsorted_matches.append((alias_distance, a_key))
+            ##  re_ci_r = re.compile(r, re.IGNORECASE)
+            ##  for a_key, a_val in aliases.items():
+            ##      for v in a_val:
+            ##          if re.match(re_ci_r, v):
+            ##          unsorted_matches.append((alias_distance, a_key))
         for i, d in enumerate(dist):
             if d < len(r) * match_threshold:
                 # Return Criteria
