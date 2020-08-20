1b3e3c3e794c789bebeedd91b781170a8123a114
Adam Bayliss <rabayliss@wisc.edu>
2020-08-20 16:45:43 -0500

    StructureOperations.py: FIX: Key that will be used in a future version to distinguish concurrent, sequential, multitarget plans was left in the required dialog. Caused KeyError in first dialog
    BeamOperations.py
    FIX: Adding TomoTherapy attributes to beam loading is interfering with normal VMAT beam loading. Putting in try/except for optional beam parameters. Caught on Attribute error for multiple beams
    FIX: Added tomotherapy attibutes to the beam object to support when they are None typ
    
    UWPelvis.xml
    FIX: Bone Marrow in clinical goals protocols was incorrectly named in clincal Goals as "Bone_Marrow", changed to BoneMarrow
    
    autoplan_whole_brain.py:
    CLEAN: inserting a blank response catch and clean exit to empty dialog
    
    dry_run.py
    CLEAN: inserting a blank response catch and clean exit to empty dialo
    TG-263.xml
    BUILD: Added alias Bone_Marrow to BoneMarrow entry
    BUILD: Add dependencies for Rectum-CTV, Brain-CTV etc..
    
    Xml_Out.py
    CLEANUP: Adding python3 print to output of xml
    
    TrackingDSP_UWmod.py
    PYTHON3: Convert print statement
    
    GenerateGoalTemplates.py
    PYTHON3: Update print statement
    
    CreateMobius3DValidation.py
    PYTHON3: Update print statement
    
    requirements.txt:
    adding python dependancies for version 2.
    
    Objectives.py
    Debug: DFO not setting low dose properly due to the relative dose tag being turned to Gy
                 exempting DFO limits from this step.
    
    UWBrainCNS_SABR.xml
    -Changed Normal_10 to Normal_1cm
    
    TG-263.xml:
    FIX: Colors added for Fiducial_PRV02
    
    UWLung.xml
    FIX: Fixed the dose limit goals on the 45 Gy BID treatment for R,L, Medi lung cases
    FIX: Putting dmax limits in, instead of volumew
    BUILD: Added StephO's BID recip

--- library/Objectives.py
+++ library/Objectives.py
@@ -374,8 +374,11 @@ def add_objective(obj, exam, case, plan, beamset,
             logging.debug('ROI: {} Protocol dose {} {} substituted with {} Gy'.format(
                 obj.find('name').text, obj.find('dose').text, obj.find('dose').attrib["units"],
                 weighted_dose))
-            if not obj.find('type').text == 'DFO':
-                obj.find('dose').attrib["units"] = "Gy"
+            if obj.find('type').text == 'DFO':
+                low_dose =  float(s_dose) * float(obj.find('dose').attrib['low']) / 100
+                obj.find('dose').attrib['low'] = low_dose
+                
+            obj.find('dose').attrib["units"] = "Gy"
             # Change the element to the substitute dose times the percentage of the reference
             obj.find('dose').text = weighted_dose
             dose = float(obj.find('dose').text) * 100
