171cc304608ded162c8d4638976caaa0b7cb9e7f
djjacqmin <dustin.jacqmin@gmail.com>
2022-01-26 17:46:01 -0600

    Add working DICOMvsDICOM

---
+++ development/DICOMvsDICOM.py
@@ -0,0 +1,271 @@
+from pathlib import Path
+import pydicom
+import numpy as np
+from DicomPairClasses import Result, ElementPair, SequencePair, DicomTreePair
+
+file_path = Path(r"U:\UWHealth\RadOnc\ShareAll\Users\ZEL\DICOM_Compare_Files\3164588")
+raystation_filename = r"RP1.2.752.243.1.1.20220110105336812.2000.10016.dcm"
+aria_filename = r"Bol_ARIA1.2.246.352.71.5.137378053967.332155.20220111111326.dcm"
+aria_filename = r"NoB_ARIA1.2.246.352.71.5.137378053967.332249.20220111111326.dcm"
+
+def returnExpectedMismatch(value_pair, comment=""):
+    return (Result.ELEMENT_EXPECTED_MISMATCH, comment)
+
+def assessNearMatch(value_pair, comment="", tolerence_value=0.01):
+    
+    # Make sure both are not None first
+    if (value_pair[0] is None) or (value_pair[1] is None):
+        return (Result.ELEMENT_MISMATCH, "Mismatch declared: At least one value is None.")
+    
+    ds1_array = np.array(value_pair[0])
+    ds2_array = np.array(value_pair[1])
+    if np.allclose(ds1_array, ds2_array, rtol=0.0, atol=tolerence_value):
+        return (Result.ELEMENT_ACCEPTABLE_NEAR_MATCH, comment)
+    else:
+        return (Result.ELEMENT_MISMATCH, comment)
+        
+
+process_function_dict = {
+    "SoftwareVersions": (returnExpectedMismatch, {}),
+    "LeafJawPositions": (assessNearMatch, {"tolerence_value": 0.01}),  # 0.01 mm
+    "TableTopLateralPosition": (returnExpectedMismatch, {}),
+    "TableTopLongitudinalPosition": (returnExpectedMismatch, {}),
+    "TableTopVerticalPosition": (returnExpectedMismatch, {}),
+}
+
+match_dict = {
+    "BeamSequence": "BeamNumber",
+    "BeamLimitingDeviceSequence": "RTBeamLimitingDeviceType",
+    "ControlPointSequence": "ControlPointIndex",
+    "BeamLimitingDevicePositionSequence": "RTBeamLimitingDeviceType",
+    "ReferencedDoseReferenceSequence": "ReferencedDoseReferenceNumber",
+    "PrimaryFluenceModeSequence": "FluenceMode",
+    "ReferencedBolusSequence": "ReferencedROINumber",
+    "DoseReferenceSequence": "DoseReferenceNumber", 
+    "FractionGroupSequence":"FractionGroupNumber",
+    "ReferencedBeamSequence": "ReferencedBeamNumber",
+    "PatientSetupSequence": "PatientSetupNumber",
+    "ReferencedStructureSetSequence":"ReferencedSOPInstanceUID",
+    "ToleranceTableSequence": "ToleranceTableNumber",
+    "BeamLimitingDeviceToleranceSequence":"RTBeamLimitingDeviceType"
+}
+
+def create_dicom_tree_pair(ds1, ds2, depth=0):
+    
+    tree_list = []
+    
+    # Loop over all keywords in the first DICOM file
+    for ds1_keyword in ds1.dir():
+        
+        if ds1_keyword in process_function_dict:
+            process_func, kwargs = process_function_dict[ds1_keyword]
+        else:
+            process_func, kwargs = None, None
+            
+        # CASE 1: The item is not a Sequence
+        if ds1[ds1_keyword].VR != "SQ":          
+            
+            if ds1_keyword not in ds2.dir():
+                value_pair = (ds1[ds1_keyword].value, None)
+            else:
+                value_pair = (ds1[ds1_keyword].value, ds2[ds1_keyword].value)
+            
+            tree_list.append(
+                ElementPair(
+                    attribute_name=ds1_keyword,
+                    value_pair=value_pair,
+                    comment="",
+                    depth=depth+1,
+                    process_func=process_func,
+                    process_func_kwargs=kwargs,
+                )
+            )
+                                
+            continue
+            
+        # CASE 2: The item is a sequence
+        
+        # CASE 2a: Sequence ds1_keyword is not in our match sequence dictionary
+        # In order to match items in a sequences, we must know which data element
+        # to use for matching. Check match_dict to see if one is specified. If not, skip it.
+        if ds1_keyword not in match_dict.keys():
+
+            tree_list.append(
+                SequencePair(
+                    attribute_name=ds1_keyword,
+                    sequence_list=[],
+                    comment="Sequence was skipped because the attribute was not found in match_dict",
+                    depth=depth+1,
+                )
+            )    
+            
+            continue
+            
+            
+        # CASE 2b: Sequence ds1_keyword is unique to dataset 1
+        # If a sequence is unique to one dataset, we could choose to do one of two thing:
+        # 1. Stop searching, and simply document the finding
+        # 2. Enter the sequence and declare everything below to be unique as well
+        # We will do the latter, as the details of the unique item may be of importance
+        # and we want to preserve that information for analysis.
+        if ds1_keyword not in ds2.dir():
+            
+            sequence_list = []
+            for item1 in ds1[ds1_keyword]:
+                sequence_list.append(
+                    create_dicom_tree_pair(item1,pydicom.Dataset(), depth=depth+2)
+                )
+                
+            tree_list.append(
+                SequencePair(
+                    attribute_name=ds1_keyword,
+                    sequence_list=sequence_list,
+                    comment="Sequence is unique to the first dataset.",
+                    depth=depth+1,
+                )
+            )
+            continue
+            
+        # CASE 2c: Sequence ds1_keyword has a match in ds2
+        
+        # Loop over items in the sequence, matching one at a time
+        # If a match is found, we will recursively send it into create_dicom_pair_dictionary()
+        match_keyword = match_dict[ds1_keyword]
+        
+        sequence_list = []
+        # This is the slowest way to find matches, but it works. 
+        # O(N) = N^2. Could probably get O(N) = 2N.
+        for item1 in ds1[ds1_keyword]:
+            found = False
+            for item2 in ds2[ds1_keyword]:
+                if item1[match_keyword].value == item2[match_keyword].value:
+                    # We found a match. Send each element tree into comparison
+                    found = True
+                    sequence_list.append(
+                        create_dicom_tree_pair(item1, item2, depth=depth+2)
+                    )
+                
+            if not found:
+                # If we don't find a match, then this sequence item is unique
+                # We will pair it with blank dataset, which will result in 
+                # all child elements being declared unique.
+                sequence_list.append(
+                    create_dicom_tree_pair(item1, pydicom.Dataset(), depth=depth+2)
+                )
+        
+        # Repeat with ds2 as the search focus, to find unique items in ds2
+        for item2 in ds2[ds1_keyword]:
+            found = False
+            for item1 in ds1[ds1_keyword]:
+                if item1[match_keyword].value == item2[match_keyword].value:
+                    # We found a match.
+                    found = True
+               
+            if not found:
+                # Unique to dataset 2
+                sequence_list.append(create_dicom_tree_pair(pydicom.Dataset(), item2, depth=depth+2))
+        
+        tree_list.append(
+            SequencePair(
+                attribute_name=ds1_keyword,
+                sequence_list=sequence_list,
+                comment="",
+                depth=depth+1,
+            )
+        )
+
+    # Loop over all keywords in the second DICOM file to capture items unique to dataset 2
+    for ds2_keyword in ds2.dir():
+        if ds2_keyword in process_function_dict:
+            process_func, kwargs = process_function_dict[ds2_keyword]
+        else:
+            process_func, kwargs = None, None
+
+        if ds2[ds2_keyword].VR != "SQ":
+            
+            # Address Unique attributes in ds2
+            if ds2_keyword not in ds1.dir():
+                value_pair = (None, ds2[ds2_keyword].value)
+        
+                tree_list.append(
+                    ElementPair(
+                        attribute_name=ds2_keyword,
+                        value_pair=value_pair,
+                        comment="",
+                        depth=depth+1,
+                        process_func=process_func,
+                        process_func_kwargs=kwargs,
+                    )
+                )
+    
+            continue
+        
+        # CASE 2a: Sequence ds2_keyword is not in our match sequence dictionary
+        if (ds2_keyword not in match_dict.keys()) and (ds2_keyword not in ds1.dir()):
+
+            tree_list.append(
+                SequencePair(
+                    attribute_name=ds2_keyword,
+                    sequence_list=[],
+                    comment="Sequence was skipped because the attribute was not found in match_dict",
+                    depth=depth+1,
+                )
+            )    
+
+            continue
+        
+        # CASE 2b: Sequence ds2_keyword is unique to dataset 2
+        if ds2_keyword not in ds1.dir():
+            
+            sequence_list = []
+            for item2 in ds2[ds2_keyword]:
+                sequence_list.append(
+                    create_dicom_tree_pair(pydicom.Dataset(), item2, depth=depth+2)
+                )
+                
+            tree_list.append(
+                SequencePair(
+                    attribute_name=ds2_keyword,
+                    sequence_list=sequence_list,
+                    comment="Sequence is unique to the second dataset.",
+                    depth=depth+1,
+                )
+            )
+            continue
+        
+    return DicomTreePair(tree_list=tree_list, depth=depth)
+
+
+def print_pair_tree_results(
+    dicom_pair_tree,
+    
+):
+    for item in dicom_pair_tree.tree_list:
+        if isinstance(item, ElementPair):
+            print(item.attribute_name, item.match_result, item.comment)
+            print("  1: ", item.value_pair[0])
+            print("  2: ", item.value_pair[1])
+
+
+def compare_dicomrt_plans(filepath1, filepath2):
+    """ Compares two DICOM-RT Plan files
+
+    PARAMETERS
+    ----------
+    filepath1: Path or string
+        The path and filename to the first DICOM-RT plan file
+    filepath2: Path or string
+        The path and filename to the second DICOM-RT plan file
+    """
+
+    ds1 = pydicom.dcmread(filepath1, force=True)
+    ds2 = pydicom.dcmread(filepath2, force=True)
+
+    dicom_pair_tree = create_dicom_tree_pair(ds1, ds2)
+
+    # print_pair_tree_results(dicom_pair_tree)
+
+    return dicom_pair_tree
+
+print(compare_dicomrt_plans(file_path / raystation_filename, file_path / aria_filename))
+
