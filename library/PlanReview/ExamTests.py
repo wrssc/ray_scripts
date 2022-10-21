03c1c3a5090d57a7653ad76576ea3ef0761a8472
rabayliss <rabayliss@wisc.edu>
2022-10-21 16:54:24 -0500

    feature(ExamTests.py): Added all patient orientations in rotation check
    Adding HFP,FFS, HFDL, HFDR, FFS, FFDR, FFDL to rotation check.
    
    Signed-off-by: rabayliss <rabayliss@wisc.edu>

--- library/PlanReview/ExamTests.py
+++ library/PlanReview/ExamTests.py
@@ -602,19 +602,44 @@ def check_localization(rso):
 
 def match_image_directions(rso):
     # Match the directions that a correctly oriented image should have
-    stack_details = {'direction_column': {'x': int(0), 'y': int(1), 'z': int(0)},
-                     'direction_row': {'x': int(1), 'y': int(0), 'z': int(0)},
-                     'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}}
+    patient_position = str(rso.exam.PatientPosition)
+
+    stack_details = {
+        'FFDL': {'direction_column': {'x': int(1), 'y': int(0), 'z': int(0)},
+                 'direction_row': {'x': int(0), 'y': int(-1), 'z': int(0)},
+                 'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+        'FFDR': {'direction_column': {'x': int(-1), 'y': int(0), 'z': int(0)},
+                 'direction_row': {'x': int(0), 'y': int(1), 'z': int(0)},
+                 'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+        'FFP': {'direction_column': {'x': int(0), 'y': int(-1), 'z': int(0)},
+                'direction_row': {'x': int(-1), 'y': int(0), 'z': int(0)},
+                'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+        'FFS': {'direction_column': {'x': int(0), 'y': int(1), 'z': int(0)},
+                'direction_row': {'x': int(1), 'y': int(0), 'z': int(0)},
+                'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+        'HFS': {'direction_column': {'x': int(0), 'y': int(1), 'z': int(0)},
+                'direction_row': {'x': int(1), 'y': int(0), 'z': int(0)},
+                'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+        'HFDL': {'direction_column': {'x': int(1), 'y': int(0), 'z': int(0)},
+                 'direction_row': {'x': int(0), 'y': int(-1), 'z': int(0)},
+                 'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+        'HFDR': {'direction_column': {'x': int(-1), 'y': int(0), 'z': int(0)},
+                 'direction_row': {'x': int(0), 'y': int(1), 'z': int(0)},
+                 'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+        'HFP': {'direction_column': {'x': int(0), 'y': int(-1), 'z': int(0)},
+                'direction_row': {'x': int(-1), 'y': int(0), 'z': int(0)},
+                'direction_slice': {'x': int(0), 'y': int(0), 'z': int(1)}},
+    }
     col_dir = rso.exam.Series[0].ImageStack.ColumnDirection
     row_dir = rso.exam.Series[0].ImageStack.RowDirection
     sli_dir = rso.exam.Series[0].ImageStack.SliceDirection
     message_str = ""
     pass_result = PASS
-    if col_dir != stack_details['direction_column'] or \
-            sli_dir != stack_details['direction_slice']:
+    if col_dir != stack_details[patient_position]['direction_column'] or \
+            sli_dir != stack_details[patient_position]['direction_slice']:
         message_str += f"Exam {rso.exam.Name} has been rotated and will not transfer to iDMS!"
         pass_result = FAIL
-    if row_dir != stack_details['direction_row']:
+    if row_dir != stack_details[patient_position]['direction_row']:
         message_str += f"Exam {rso.exam.Name} has been rotated or was acquired" \
                        + " with gantry tilt and should be reoriented!"
         pass_result = FAIL
