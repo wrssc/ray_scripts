0ff5833760e248392219ed5f3aad39496ae3cc2f
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-06-16 12:17:04 -0500

    🔨 UW_Definitions.py: Refactor indentation and comments
    This commit refactors the indentation and comments in UW_Definitions.py file. The 'SUPPORTS' list has been updated to include 'S-frame' and the comments have been improved for 'DIAMETER' and 'COLLISION_TOLERANCE'. This improves code readability and maintainability.
    
    Signed-off-by: Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>

--- library/UW_Definitions.py
+++ library/UW_Definitions.py
@@ -1,9 +1,9 @@
 TOMO_DATA = {'MACHINES': ['HDA0488'],
              'PLAN_TR_SUFFIX': r'_Tr',
              'LATERAL_ISO_MARGIN': 2.,  # cm
-             'SUPPORTS': ['TomoCouch','S-frame'],
-             'DIAMETER': 85.0, # cm cover diameter
-             'COLLISION_TOLERANCE': 2.0, # cm distance tolerance for shifting
+             'SUPPORTS': ['TomoCouch', 'S-frame'],
+             'DIAMETER': 85.0,  # cm cover diameter
+             'COLLISION_TOLERANCE': 2.0,  # cm distance tolerance for shifting
              }
 
 TRUEBEAM_DATA = {'MACHINES': ['TrueBeam', 'TrueBeamSTx'],
