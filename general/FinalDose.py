d3e424bf6a04c4474814facc9f030d4cd7a96cb4
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-06-21 14:32:53 -0500

    🛠️ FinalDose.py: Added support for Fiducials as ROIs
    - Add any ROIs labeled Fiducials to the list of included patterns for export.
    
    Signed-off-by: Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>

--- general/FinalDose.py
+++ general/FinalDose.py
@@ -138,7 +138,8 @@ def process_rois_for_export(plan, case):
             rois_for_export.append(r)
 
     # Add ROIs containing "block" to the export list
-    include_patterns = [r'(?i)\b\w*block\w*\b']
+    # Add any ROIs labeled Fiducials
+    include_patterns = [r'(?i)\b\w*block\w*\b', r'(?i)\b\w*fiducial\w*\b']
     for r in case.PatientModel.RegionsOfInterest:
         if StructureOperations.any_regex_match(include_patterns, r.Name):
             rois_for_export.append(r.Name)
