95ce48baf0116e4589ec0910b1ef67a2f3cd1a7b
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-09-30 08:15:13 -0500

    :recycle: run_physics_review.py:
    Updated import statement for physics_review module
    
    * Changed import path from 'PlanReview.PlanReview' to 'PlanReview.physics_review'

--- testing/run_physics_review.py
+++ testing/run_physics_review.py
@@ -7,6 +7,6 @@ from pathlib import Path
 
 plan_review_path = Path(__file__).parent.parent / "library" / "PlanReview"
 sys.path.insert(1, str(plan_review_path))
-from PlanReview.PlanReview import physics_review
+from PlanReview.physics_review import physics_review
 
 physics_review(rso=None, do_physics_review=True)
