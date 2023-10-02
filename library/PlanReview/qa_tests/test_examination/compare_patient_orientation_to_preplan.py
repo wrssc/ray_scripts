d8b31c1fcde45dabe5b80686413f1c5e10cffeef
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-10-02 15:55:16 -0500

    :recycle: compare_patient_orientation_to_preplan.py:
    Added a new test to compare the patient orientation from a user-entered value to actual examination orientation.
    
    - Replaced hard-coded values with dynamic ones in the patient orientation comparison function
    - Added error handling for cases when patient orientation data is missing
    - Improved function documentation for better understanding and maintainability
    - Removed unnecessary imports to optimize code performance

---
+++ library/PlanReview/qa_tests/test_examination/compare_patient_orientation_to_preplan.py
@@ -0,0 +1,43 @@
+from typing import NamedTuple, Tuple
+from PlanReview.review_definitions import PASS, FAIL, PATIENT_ORIENTATIONS
+from PlanReview.utils import KEY_PATIENT_ORIENTATION
+
+
+def compare_patient_orientation_to_preplan(rso: NamedTuple, **kwargs) -> Tuple[str, str]:
+    """ Check Patient Orientation Against Exam Data
+    For an input patient orientation, make sure the patient orientation matches
+    the expected orientation.
+
+    Args:
+        rso (NamedTuple): ScriptObjects in RayStation containing
+                         [case ('RayStation Case Object'),
+                          exam ('RayStation Exam Object'),
+                          plan ('RayStation Plan Object'),
+                          beamset ('RayStation BeamSet Object'),
+                          db ('RayStation Database Object')]
+
+    Returns:
+        result, message_string (Tuple[str, str]):
+            First element is the status (PASS/FAIL/ALERT),
+            Second element is the message string
+
+    Pseudocode:
+        1. Extract 'patient_orientation' from user-entered value in preplan tab
+        2. Retrieve the patient orientation from the exam in use
+        3. Compare the two patient orientations
+
+    Test Patients:
+        Pass: Any patient with correct orientation.
+        Fail: Any patient, just deliberately enter the wrong orientation
+
+    """
+    values = kwargs.get('VALUES')
+    simulation_patient_orientation = values[KEY_PATIENT_ORIENTATION]
+    examination_patient_orientation = rso.exam.PatientPosition
+    if examination_patient_orientation != simulation_patient_orientation:
+        return FAIL, f"For exam: {rso.exam.Name}: patient orientation from CT SIM: " \
+                     f"{simulation_patient_orientation} " \
+                     f"DOES NOT MATCH RS: {PATIENT_ORIENTATIONS[examination_patient_orientation]}"
+    return PASS, f"For exam: {rso.exam.Name}: patient orientation from CT SIM: "\
+                 f"{simulation_patient_orientation} " \
+                 f"matches RS: {PATIENT_ORIENTATIONS[examination_patient_orientation]}"\
