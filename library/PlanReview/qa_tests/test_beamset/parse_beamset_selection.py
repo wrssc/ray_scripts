3d00adb6eb67c9001c502a86f94c8b1a3aa5a379
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-11-19 16:26:26 -0600

    :zap: parse_beamset_selection.py:
    Refactored parse_beamset_selection.py to improve readability and efficiency.
    
    * Replaced the function `parse_beamset_selection(beamset_name, messages)` with `parse_beamset_selection(rso, **kwargs)`.
    * Introduced new helper functions `determine_energy(energy_str)`, `determine_iso_method(iso_str)`, and `extract_match(regex, text)` to simplify the main function.
    * Changed the return type from dictionary to tuple, now returning a tuple containing the pass result and a message.
    * Updated the regular expressions used for parsing log messages.
    * Changed the way log messages are parsed, now looping through `log_messages` instead of `messages`.
    * Removed the initialization of `template_data` dictionary, and replaced it with default setup for `pass_result` and `message`.
    * Updated the extraction of template information from the log messages.

--- library/PlanReview/qa_tests/test_beamset/parse_beamset_selection.py
+++ library/PlanReview/qa_tests/test_beamset/parse_beamset_selection.py
@@ -1,59 +1,90 @@
 import re
+from PlanReview.review_definitions import ALERT, PASS
 
 
-def parse_beamset_selection(beamset_name, messages):
+def determine_energy(energy_str):
+    energy_regex = re.compile(r"Energy:\s?([^,]*)")
+    energy_match = energy_regex.search(energy_str)
+    if energy_match and energy_match.group(1).strip():
+        # If there is some text before the comma, return it
+        return f'Energy: {energy_match.group(1).strip()}'
+    else:
+        # If the field is empty or contains only whitespace
+        return ''
+
+
+def determine_iso_method(iso_str):
+    # Regular expressions to extract the values for ISO_0, ROI, and POI
+    iso_0_regex = re.compile(r"ISO_0':\s*'([^']*)'")
+    roi_regex = re.compile(r"ROI':\s*'([^']*)'")
+    poi_regex = re.compile(r"POI':\s*'([^']*)'")
+
+    # Extract values
+    iso_0_value = iso_0_regex.search(iso_str)
+    roi_value = roi_regex.search(iso_str)
+    poi_value = poi_regex.search(iso_str)
+
+    # Check each key and return the method with its value
+    if iso_0_value and iso_0_value.group(1):
+        return f'Existing Isocenter: {iso_0_value.group(1)}'
+    elif roi_value and roi_value.group(1):
+        return f'Center of Roi: {roi_value.group(1)}'
+    elif poi_value and poi_value.group(1):
+        return f'At Poi: {poi_value.group(1)}'
+    return ''
+
+
+def extract_match(regex, text):
+    """Helper function to extract match using regex."""
+    match = re.search(regex, text)
+    return match.group(1) if match else None
+
+
+def parse_beamset_selection(rso, **kwargs):
     """
-    Parse the messages for a specific beamset dialog and return the dialog
-    choices as a dictionary.
+    Parse the log_messages for a specific beamset dialog and return the dialog choices as a dictionary.
 
     Args:
-        beamset_name (str): The name of the beamset to look for in the messages.
-        messages (list): A list of messages to search through.
+        rso (NamedTuple): A namedtuple containing the beamset.
+        **kwargs: Additional keyword arguments. Options include:
+            'LOG_MESSAGES' (Optional[list]): A list of log_messages to search through.
 
     Returns:
-        A dictionary containing the beamset template dialog choices.
+        tuple: A tuple containing the pass result and a message.
     """
+    log_messages = kwargs.get('LOG_MESSAGES', [])
+    beamset_name = rso.beamset.DicomPlanLabel
+    beamset_id = rso.beamset.UniqueId
 
-    # Define regular expressions to search for in the messages
-    beamset_template_searches = {
-        'Dialog': re.compile(r'(Dialog):\s?(Beamset Template Selection)'),
-        'Name': re.compile(r'(TemplateName):\s?(.*)Iso'),
-        'NameAlt': re.compile(r'(TemplateName):\s?([^\t]+)'),
-        'Isocenter': re.compile(r'(Iso):\s?([^\t]+)'),
-        'Energy': re.compile(r'(Energy):\s?([^\t]+)')
+    # Regular expressions for parsing log messages
+    regexes = {
+        'Dialog': re.compile(r'Dialog:\s?Beamset Template Selection'),
+        'Name': re.compile(r'TemplateName:\s?(.*?)(?=\s*,\t)'),
+        'Isocenter': re.compile(r'Iso:\s?\{([^}]+)\}'),
+        'Energy': re.compile(r'Energy:\s?(.*?)(?=,\t|$)')
     }
 
-    # Initialize the template_data dictionary with default values
-    template_data = {
-        'Beamset Template Selection': (
-            None, 'Beamset {} not set by script'.format(beamset_name)),
-        'Template Name': (None, ''),
-        'Isocenter': (None, ''),
-        'Energy': (None, '')
-    }
+    # Default result setup
+    pass_result = ALERT
+    message = f'Beamset {beamset_name} not set by script'
 
-    # Search through the messages for a beamset dialog matching the beamset name
-    for message in messages:
-        if re.search(beamset_template_searches['Dialog'],
-                     message[3]) and beamset_name in message[1]:
-            # Found a matching beamset dialog: extract the template information
-            try:
-                _, template_name = re.search(beamset_template_searches['Name'],
-                                             message[3]).groups()
-            except AttributeError:
-                _, template_name = re.search(
-                    beamset_template_searches['NameAlt'], message[3]).groups()
-            _, iso_info = re.search(beamset_template_searches['Isocenter'],
-                                    message[3]).groups()
-            _, energy = re.search(beamset_template_searches['Energy'],
-                                  message[3]).groups()
+    # Parsing log messages
+    for log_line in log_messages:
+        if 'Beamset Template Selection' in log_line['Message'] and \
+                (beamset_name == log_line['Beamset'] or beamset_id in log_line['BeamsetID']):
 
-            # Update the template_data dictionary with the extracted information
-            template_data['Beamset Template Selection'] = (None, message[1])
-            template_data['Template Name'] = (None, template_name)
-            # TODO: Replace with a parse on bracketed entries return
-            #  placement method
-            template_data['Isocenter'] = (None, iso_info)
-            template_data['Energy'] = (None, energy)
+            template_name = extract_match(regexes['Name'], log_line['Message'])
+            iso_info = extract_match(regexes['Isocenter'], log_line['Message'])
+            energy = extract_match(regexes['Energy'], log_line['Message'])
 
-    return template_data
+            if template_name:
+                iso_method = determine_iso_method(iso_info)
+                energy_string = determine_energy(energy)
+                pass_result = PASS
+                message = f'Template: {template_name}, Isocenter: {iso_method}, Energy: {energy_string}'
+
+    return pass_result, message
+
+
+
+
