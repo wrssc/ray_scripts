0cb1f77742d904a6f903b3745a148cc391ae73e3
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-11-19 16:21:47 -0600

    :bulb: progress_bar_tests.py:
    Refactored display_progress_bar function for better customization
    
    * Changed the function definition to accept two new arguments: 'title_text' and 'progress_bar_text' for customizing the window title and progress bar text respectively
    * Added a key 'progress_text' to the Sg.Text element in the layout to allow dynamic updates to the progress bar text
    * Added a new variable 'progress_text' to capture the 'progress_text' element from the window for further use
    * Updated the function return statement to include the new 'progress_text' variable

--- library/PlanReview/guis/progress_bar_tests.py
+++ library/PlanReview/guis/progress_bar_tests.py
@@ -1,15 +1,16 @@
 import PySimpleGUI as Sg
 
 
-def display_progress_bar():
-    layout = [[Sg.Text('Running tests...')],
+def display_progress_bar(title_text='Progress on tests', progress_bar_text='Running tests...'):
+    layout = [[Sg.Text(progress_bar_text, key='progress_text')],
               [Sg.ProgressBar(max_value=100, orientation='h', size=(30, 20),
                               key='progressbar')]]
 
-    window = Sg.Window('Progress', layout, no_titlebar=True,
+    window = Sg.Window(title_text, layout, no_titlebar=True,
                        keep_on_top=True, finalize=True)
 
     progress_bar = window['progressbar']
+    progress_text = window['progress_text']
     progress_bar.UpdateBar(0)
 
-    return window, progress_bar
+    return window, progress_bar, progress_text
