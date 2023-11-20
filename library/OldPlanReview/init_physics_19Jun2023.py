88ba32e13dcc6daf4027a099fdcbee08e9f69eaa
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-11-20 12:50:21 -0600

    :recycle: init_physics_19Jun2023.py:
    Refactored log file reading and parsing functions and improved log message handling.
    
    * Imported `retrieve_logs` function for log retrieval.
    * Replaced `read_log_file` function with `parse_order_selection`.
    * Removed `parse_log_file` and `parse_beamset_selection` functions.
    * Added `log_messages` parameter to `get_beamset_level_tests` function.
    * Simplified condition checks for `patient_checks_dict`, `plan_checks_dict` and `beamset_checks_dict`.
    * Removed `check_patient_logs` logic and replaced with `log_messages` handling.
    * Enhanced log message insertion into `treedata` with parent node checking.
    * Improved string formatting and line breaks for better readability.

--- library/OldPlanReview/init_physics_19Jun2023.py
+++ library/OldPlanReview/init_physics_19Jun2023.py
@@ -115,6 +115,7 @@ from BeamSetReviewTests import *
 from PlanReviewTests import *
 from ReviewDefinitions import *
 import GeneralOperations
+from retrieve_logs import retrieve_logs
 
 
 def comment_to_clipboard(rso):
@@ -134,96 +135,10 @@ def comment_to_clipboard(rso):
     return r
 
 
-def read_log_file(patient_id):
+def parse_order_selection(beamset_name, messages, dialog_key):
     """
-    Read the lines from the patient log in both clinical and development locations
-    Args:
-        patient_id: str: contains patient ID
-
-    Returns:
-        file_contents: lines of file
-    """
-    log_file = patient_id + '.txt'
-    log_input_file = os.path.join(LOG_DIR, patient_id, log_file)
-    dev_log_file = patient_id + '.txt'
-    dev_log_input_file = os.path.join(DEV_LOG_DIR, patient_id, dev_log_file)
-    try:
-        with open(log_input_file) as f:
-            file_contents = f.readlines()
-    except FileNotFoundError:
-        logging.debug("File {} not found in dir {}"
-                      .format(log_file, LOG_DIR))
-        file_contents = []
-    try:
-        with open(dev_log_input_file) as f:
-            file_contents.append(f.readlines())
-    except FileNotFoundError:
-        logging.debug("File {} not found in dir {}"
-                      .format(dev_log_file, DEV_LOG_DIR))
-    return file_contents
-
-
-def parse_log_file(lines, parent_key, phrases=KEEP_PHRASES):
-    """
-    Parse the log file lines for the phrase specified
-    Args:
-        parent_key: The top key for these log entries (typically patient level)
-        lines: list of strings from a log file
-        phrases: list of tuples
-            (level,phrase):
-                           level: is a string indicating pass level
-                           phrase: is a string to identify lines for return
-
-    Returns:
-        message: list of lists of format: [parent key, key, value, result]
-
-    Test Patient:
-        Script_Testing^Plan_Review, #ZZUWQA_ScTest_01May2022, Log_Parse_Check
-    """
-    message = []
-    time_stamp_exp = r'(^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})(.*)'  # The front timestamp
-    re_c = r'^\t(Case: .*)\t'
-    re_e = r'(Exam: .*)\t'
-    re_p = r'(Plan: .*)\t'
-    re_b = r'(Beamset: [^\t|\s]+)'  # Terminate after this level
-    #
-    # Declare reg-ex for levels in the log file
-    context_exps = OrderedDict()
-    context_exps['Beamset'] = re_c + re_e + re_p + re_b
-    context_exps['Plan'] = re_c + re_e + re_p
-    context_exps['Exam'] = re_c + re_e
-    context_exps['Case'] = re_c
-    for p in phrases:
-        key = 'Log' + p[1]
-        message.append([parent_key, key, key, p[0]])
-        re_phrase = p[1] + r'.*\.py: '
-        for l in lines:
-            if p[1] in l:
-                l = re.sub(re_phrase, '', l)  # Wipe out the source python program
-                # Sort the line into a part for the timestamp and one for remainder
-                parsed_l = [part for t in re.findall(time_stamp_exp, l) for part in t]
-                parsed_l[1].lstrip()  # front white space
-                parsed_l[1].rstrip()  # Remove \n
-                deepest_level = None
-                for c, exp in context_exps.items():
-                    if bool(re.search(exp, parsed_l[1])):
-                        levels = OrderedDict()
-                        deepest_level = c
-                        for g in re.findall(exp, parsed_l[1])[0]:
-                            lev_key, lev_val = re.split(': ', g, maxsplit=1)
-                            levels[lev_key] = lev_val
-                        parsed_l[1] = re.sub(exp, '', parsed_l[1])
-                        parsed_l[0] += ' ' + deepest_level + ': ' + levels[deepest_level]
-                        break
-                if not deepest_level:
-                    parsed_l[1] = re.sub(r'\t', '', parsed_l[1])
-                message.append([key, parsed_l[0], parsed_l[0], parsed_l[1]])
-    return message
-
-
-def parse_treatment_planning_order_selection(beamset_name, messages, dialog_key):
-    """
-    Using the beamset name and the log file, use regex to find a phrase identifying the
+    Using the beamset name and the log file, use regex to find a phrase
+    identifying the
     template name used in the treatment planning order for this beamset.
     Args:
         beamset_name: <str> name of current beamset
@@ -232,58 +147,22 @@ def parse_treatment_planning_order_selection(beamset_name, messages, dialog_key)
 
     Returns:
 
-    TODO: Take a reg-exp as a list for input for matching a dialog and for each desired phrase
-          loop over the phrases for a match
-    TODO: Add the target matching that takes place for this step with consideration of the
-          pre-logcrit syntax and post-logcrit syntax
-
 
     """
     # Parse the dialogs for specific key phrases related to the beamset dialog
     beamset_template_searches = {'Dialog': re.compile(
         r'(Treatment Planning Order selected:\s?)(.*)$')}
     template_data = {dialog_key: (ALERT,
-                                  f'Treatment Planning for Beamset {beamset_name} goals manually defined')}
+                                  f'Treatment Planning for Beamset '
+                                  f'{beamset_name} goals manually defined')}
     for m in messages:
-        template_search = re.search(beamset_template_searches['Dialog'], m[3])
-        if template_search and beamset_name in m[1]:
+        template_search = re.search(beamset_template_searches['Dialog'], m['Message'])
+        if template_search and beamset_name in m['Message']:
             # Found the TPO Dialog. Lets display it
-            # Note that it is a little sloppy, since this will always grab the last match in the log file
+            # Note that it is a little sloppy, since this will always grab
+            # the last match in the log file
             k, template_name = template_search.groups()
             template_data[dialog_key] = (None, template_name)
-    return template_data
-
-
-def parse_beamset_selection(beamset_name, messages, dialog_key):
-    # Go in here and if the message line matches the beamset_name
-    # then return all the choices in this dialog as a list of tuples
-
-    # Parse the dialogs for specific key phrases related to the beamset dialog
-    beamset_template_searches = {'Dialog': re.compile(r'(Dialog):\s?(Beamset Template Selection)'),
-                                 'Name': re.compile(r'(TemplateName):\s?(.*)Iso'),
-                                 'NameAlt': re.compile(r'(TemplateName):\s?([^\t]+)'),
-                                 'Isocenter': re.compile(r'(Iso):\s?([^\t]+)'),
-                                 'Energy': re.compile(r'(Energy):\s?([^\t]+)')}
-    # Find all the message lines that relate to a beamset dialog
-    dialog_key = 'Beamset Template Selection'
-    template_data = {dialog_key: (ALERT, 'Beamset {} not set by script'.format(beamset_name))}
-    # loop over all lines in the template
-    for m in messages:
-        if re.search(beamset_template_searches['Dialog'], m[3]) and beamset_name in m[1]:
-            # This is a beamset dialog: add it to the beamset listings
-            # Get the template name
-            try:
-                k, template_name = re.search(beamset_template_searches['Name'], m[3]).groups()
-            except AttributeError:
-                k, template_name = re.search(beamset_template_searches['NameAlt'], m[3]).groups()
-            # Get the isocenter information
-            k, iso_info = re.search(beamset_template_searches['Isocenter'], m[3]).groups()
-            k, energy = re.search(beamset_template_searches['Energy'], m[3]).groups()
-            template_data[dialog_key] = (None, m[1])
-            template_data['Template Name'] = (None, template_name)
-            # TODO: Replace with a parse on bracketed entries return placement method
-            template_data['Isocenter'] = (None, iso_info)
-            template_data['Energy'] = (None, energy)
     return template_data
 
 
@@ -375,10 +254,15 @@ def get_plan_level_tests(rso, physics_review=True):
     return plan_checks_dict
 
 
-def get_beamset_level_tests(rso, physics_review=True):
+def get_beamset_level_tests(rso, physics_review=True, log_messages=None):
+    if log_messages is None:
+        log_messages = retrieve_logs(rso)
+
     beamset_checks_dict = {
         "Beamset approval status":
             (BeamSetReviewTests.check_beamset_approved, {"do_physics_review": physics_review}),
+        "Beamset Template Selection":
+            (BeamSetReviewTests.parse_beamset_selection, {'LOG_MESSAGES': log_messages}),
         "Isocenter Position Identical":
             (BeamSetReviewTests.check_common_isocenter, {"tolerance": 1e-15}),
         "Check Fractionation":
@@ -492,27 +376,19 @@ def check_plan(physics_review=True):
     """
     Gather Patient Level Checks
     """
-    if rso.exam:
-        patient_checks_dict = get_exam_level_tests(rso)
-    check_patient_logs = True if rso.patient else False
-    plan_checks_dict = {}
+    patient_checks_dict = get_exam_level_tests(rso) if rso.exam else {}
     """
     Gather Plan Level Checks
     """
-    if rso.plan:
-        plan_checks_dict = get_plan_level_tests(rso, physics_review)
-    #
+    plan_checks_dict = get_plan_level_tests(rso, physics_review) if rso.plan else {}
     """
     Gather BeamSet Level Checks
     """
-    if rso.beamset:
-        beamset_checks_dict = get_beamset_level_tests(rso, physics_review)
+    log_messages = retrieve_logs(rso) if rso.beamset else []
+    beamset_checks_dict = get_beamset_level_tests(rso, physics_review, log_messages) if rso.beamset else {}
     """
     Parse logs
     """
-    if check_patient_logs:
-        lines = read_log_file(patient_id=rso.patient.PatientID)
-        message_logs = parse_log_file(lines=lines, parent_key=log_key[0], phrases=KEEP_PHRASES)
     # Execute qa_tests
     exam_level_tests = []
     for key, p_func in patient_checks_dict.items():
@@ -538,29 +414,11 @@ def check_plan(physics_review=True):
     #
     # BEAMSET LEVEL CHECKS
     beamset_level_tests = []
-    #
-    # Run dialog parse
-    dialog_key = 'Beamset Template Selection'
-    beamset_dialog = parse_beamset_selection(beamset_name=rso.beamset.DicomPlanLabel,
-                                             messages=message_logs,
-                                             dialog_key=dialog_key)
-    message = build_tree_element(parent_key=beamset_key[0],
-                                 child_key=dialog_key,
-                                 pass_result=beamset_dialog[dialog_key][0],
-                                 message_str=beamset_dialog[dialog_key][1])
-    beamset_level_tests.extend(message)
-    for k, v in beamset_dialog.items():
-        if k != dialog_key:
-            message = build_tree_element(parent_key=dialog_key,
-                                         child_key=k,
-                                         pass_result=v[0],
-                                         message_str=v[1])
-            beamset_level_tests.extend(message)
     # Read the treatment planning order selection
     dialog_key = 'Treatment Planning Order Selection'
-    tpo_dialog = parse_treatment_planning_order_selection(beamset_name=rso.beamset.DicomPlanLabel,
-                                                          messages=message_logs,
-                                                          dialog_key=dialog_key)
+    tpo_dialog = parse_order_selection(beamset_name=rso.beamset.DicomPlanLabel,
+                                       messages=log_messages,
+                                       dialog_key=dialog_key)
     message = build_tree_element(parent_key=beamset_key[0],
                                  child_key=dialog_key,
                                  pass_result=tpo_dialog[dialog_key][0],
@@ -584,8 +442,8 @@ def check_plan(physics_review=True):
     # TREE BUILDING
     #
     # Insert main (patient) node
-    patient_level_pass, patient_icon = parse_level_tests(exam_level_tests+plan_level_tests+beamset_level_tests)
-    treedata.Insert("", patient_key[0], patient_key[1], patient_level_pass,icon=patient_icon)
+    patient_level_pass, patient_icon = parse_level_tests(exam_level_tests + plan_level_tests + beamset_level_tests)
+    treedata.Insert("", patient_key[0], patient_key[1], patient_level_pass, icon=patient_icon)
     # Insert Exam Level Nodes
     exam_level_pass, exam_icon = parse_level_tests(exam_level_tests)
     treedata.Insert(patient_key[0], exam_key[0], exam_key[1], exam_level_pass, icon=exam_icon)
@@ -602,9 +460,16 @@ def check_plan(physics_review=True):
     #
     # Log Level
     treedata.Insert(patient_key[0], log_key[0], log_key[1], "")
-    if check_patient_logs:
-        for m in message_logs:
-            treedata.Insert(m[0], m[1], m[2], [m[3]])  # Note the list of the last entry. Can this be of use?
+    # Log Level
+    parent_nodes = set()
+    if log_messages:
+        for m in log_messages:
+            # Check if this parent node already exists
+            if m['Log_Level'] not in parent_nodes:
+                treedata.Insert(log_key[0], m['Log_Level'], m['Log_Level'], "")
+                parent_nodes.add(m['Log_Level'])
+            treedata.insert(m['Log_Level'], f"{m['Date']} {m['Time']}",
+                             f"{m['Date']} {m['Time']} {m['Source']}", [m['Message']])
 
     # Gui
     # TODO is it possible to specify which columns are expanded by pass fail?
