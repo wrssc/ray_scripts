d9c7cb4438f8033d9bad66d36975626ded4f9721
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2023-12-05 17:30:24 -0600

    :zap: get_sandbox_level_tests.py, __init__.py, check_max_dose_point.py:
    Added functionality to check maximum dose point in targets.
    
    * Imported `check_max_dose_point` function in `get_sandbox_level_tests.py` and `__init__.py`.
    * Created new file `check_max_dose_point.py` with multiple functions.
      * Added function `is_roi` to check if a region of interest (ROI) has contours.
      * Added function `sort_rois` to sort ROIs based on their types.
      * Added function `check_bounding_box_for_roi` to check if a point lies within the bounding box of an ROI.
      * Added function `is_point_in_bounding_box` to check if a point is within a bounding box.
      * Added function `is_point_inside_polygon_2d` to check if a 2D point is inside a 2D polygon.
      * Added function `find_contours_containing_point` to find contours containing a point.
      * Added function `get_slice_thickness` to get the thickness of a slice.
      * Added function `get_beamset_dose_at_point` to determine the maximum dose at a specified point.
      * Added function `check_rx_versus_primary` to check if the test prescription is the same as the primary prescription.
      * Added function `safe_get_attribute` to safely get attributes.
      * Added function `get_beamset_prescriptions` to get all prescriptions and return them as a list of dictionaries.
      * Added function `primary_rx_dose` to get the primary prescription dose.
      * Added function `normalize_rx_dose` to normalize the prescription dose.
      * Added function `get_target_rois` to collect all ROIs from beamset prescriptions and other targets.
      * Added function `check_max_dose_point` to examine the maximum dose point and return an alert if it is outside of all targets.
      * Added function `format_max_dose_message` to format the message when the max dose point is inside an ROI.
      * Added function `format_max_dose_message_outside_targets` to format the message when the max dose point is outside all ROIs.

--- library/PlanReview/qa_tests/test_sandbox/get_sandbox_level_tests.py
+++ library/PlanReview/qa_tests/test_sandbox/get_sandbox_level_tests.py
@@ -1,5 +1,6 @@
 from .check_isocenter_clearance import check_isocenter_clearance
 from .compute_vmat_beam_properties import compute_vmat_beam_properties
+from .check_max_dose_point import check_max_dose_point
 from PlanReview.review_definitions import REVIEW_LEVELS
 
 
@@ -11,6 +12,8 @@ def get_sandbox_level_tests(rso, physics_review=True):
     sandbox_checks_dict = {
         f"{REVIEW_LEVELS['SANDBOX']}::Couch Zero Full Rotation Clearance Check":
             (check_isocenter_clearance, {}),
+        f"{REVIEW_LEVELS['SANDBOX']}::Maximum Dose Inside Targets":
+            (check_max_dose_point, {}),
         f"{REVIEW_LEVELS['SANDBOX']}::Beamset Complexity":
             (compute_vmat_beam_properties, {})
     }
