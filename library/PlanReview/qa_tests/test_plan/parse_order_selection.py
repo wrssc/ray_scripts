54c644f728755e420f660aa9ee99a9a600bef9d9
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2024-01-04 14:26:51 -0600

    :art: parse_order_selection.py:
    Fixed a bug causing a missed reg-ex match on Order selection.
    
    - Changed import statement from 'logging' to 'PASS' from PlanReview.review_definitions
    - Modified 'messages' from list of strings to a dictionary of log data
    - Improved readability of 'beamset_template_searches' dictionary
    - Changed condition in 'if' statement to check 'Beamset' in 'm' instead of 'Message'
    - Updated 'template_data' dictionary to include 'PASS' instead of 'None'

--- library/PlanReview/qa_tests/test_plan/parse_order_selection.py
+++ library/PlanReview/qa_tests/test_plan/parse_order_selection.py
@@ -1,6 +1,5 @@
 import re
-from PlanReview.review_definitions import ALERT
-import logging
+from PlanReview.review_definitions import ALERT, PASS
 
 
 def parse_order_selection(beamset_name, messages, dialog_key):
@@ -10,7 +9,9 @@ def parse_order_selection(beamset_name, messages, dialog_key):
     template name used in the treatment planning order for this beamset.
     Args:
         beamset_name: <str> name of current beamset
-        messages: [ <str> ]: list of strings
+        messages: a dictionary of log data. See retrieve_logs.py
+         Keys are: 'Date', 'Time', 'Log_Level', 'Message', 'Deepest_Context',
+            'Case', 'Exam', 'Plan', 'Beamset', 'PlanID', 'BeamsetID', 'Source'
         dialog_key: key to be used for top level entry in the tree
 
     Returns:
@@ -18,17 +19,17 @@ def parse_order_selection(beamset_name, messages, dialog_key):
 
     """
     # Parse the dialogs for specific key phrases related to the beamset dialog
-    beamset_template_searches = {'Dialog': re.compile(
-        r'(Treatment Planning Order selected:\s?)(.*)$')}
+    beamset_template_searches = {
+        'Dialog': re.compile(r'(Treatment Planning Order selected:\s?)(.*)$')}
     template_data = {dialog_key: (ALERT,
                                   f'Treatment Planning for Beamset '
                                   f'{beamset_name} goals manually defined')}
     for m in messages:
         template_search = re.search(beamset_template_searches['Dialog'], m['Message'])
-        if template_search and beamset_name in m['Message']:
+        if template_search and beamset_name in m['Beamset']:
             # Found the TPO Dialog. Lets display it
             # Note that it is a little sloppy, since this will always grab
             # the last match in the log file
             k, template_name = template_search.groups()
-            template_data[dialog_key] = (None, template_name)
+            template_data[dialog_key] = (PASS, template_name)
     return template_data
