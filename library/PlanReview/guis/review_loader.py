50a7050d3214f65cae107a6d67a7066e8b345c3e
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2024-10-08 11:52:01 -0500

    🗑️ review_loader.py: Remove deprecated review loading function
    Eliminated the old review loading mechanism.
    
    - Removed `old_load_review` function which was not being used anymore.
    - This includes the removal of file reading, JSON parsing, and GUI state updates related to the old function.

--- library/PlanReview/guis/review_loader.py
+++ library/PlanReview/guis/review_loader.py
@@ -86,35 +86,3 @@ def load_review(gui_state_manager):
     if gui_state_manager.qa_form_accessible:
         load_qa_form(gui_state_manager.window, values)
 
-def old_load_review(gui_state_manager):
-    if not gui_state_manager.review_file_name:
-        gui_state_manager.review_file_name = f"{gui_state_manager.rso.patient.PatientID}_" \
-                    f"{gui_state_manager.rso.beamset.DicomPlanLabel}{gui_state_manager.suffix}"
-    try:
-        with open(os.path.join(OUTPUT_DIR, gui_state_manager.rso.patient.PatientID,
-                               gui_state_manager.review_file_name), "r") as f:
-            values = json.load(f)
-    except FileNotFoundError:
-        Sg.popup("No saved review found!")
-        return
-
-    values = str_key_to_tuple(values)
-    # Add missing keys to the window.key_dict
-    update_window_key_dict(gui_state_manager.window, values.keys())
-    # Load preplan frame contents
-    load_preplan(gui_state_manager.window, values,
-                 gui_state_manager.sites,
-                 gui_state_manager.protocols,
-                 gui_state_manager.instructions,
-                 gui_state_manager.maximum_beamset_count,
-                 gui_state_manager.maximum_target_number,
-                 gui_state_manager.rso,
-                 gui_state_manager.review_type)
-    # Load the manual (check box) tab contents
-    load_manual(gui_state_manager.window, values, gui_state_manager.check_box_copy)
-    # Load the main window data.
-    load_side_panel(gui_state_manager.window, values, gui_state_manager.review_type)
-    # Load the QA form
-    if gui_state_manager.qa_form_accessible:
-        load_qa_form(gui_state_manager.window, values)
-
