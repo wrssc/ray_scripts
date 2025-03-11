d3332d038d289880a76482ff218062f923def68d
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2025-03-11 10:38:07 -0500

    ✨ tbi_definitions.py:
    Add comprehensive constants and configurations for TBI protocols
    
    - Imported `math` and `os` modules to support mathematical operations and file path manipulations.
    - Defined constants for folder paths, including `PROTOCOL_FOLDER`, `INSTITUTION_FOLDER`, `AUTOPLAN_FOLDER`, and constructed `PATH_PROTOCOLS`.
    - Added `PATH_TO_OUTPUT` and `DICOM_PATH` for output and DICOM storage paths.
    - Introduced protocol and order names for Tomo and VMAT techniques, such as `PROTOCOL_FILE_TOMO`, `PROTOCOL_NAME_TOMO`, and `ORDER_NAME_FFS_TOMO`.
    - Defined beamset templates and machine identifiers for Tomo and VMAT, e.g., `BEAMSET_TEMPLATE_FFS_TOMO` and `TOMO_MACHINE`.
    - Configured order names and target names for different anatomical positions and techniques.
    - Set default voxel size for imaging.
    - Established treatment length parameters and calculations for both FFS and HFS, including overlaps, overshoots, and shift buffers.
    - Defined constants for junction points, points of interest (POI), and external setup configurations.
    - Added avoidance structures and margins for skin, lungs, and kidneys.
    - Specified target evaluation names and lists for FFS and HFS.
    - Introduced `MBS_ROIS` dictionary for region of interest (ROI) configurations with case types, model names, and colors.
    - Defined a list of `COLORS` for visualization purposes.

---
+++ library/tbi_autoplanning/tbi_definitions.py
@@ -0,0 +1,118 @@
+import math
+import os
+
+PROTOCOL_FOLDER = r'../../protocols'
+INSTITUTION_FOLDER = r'UW'
+AUTOPLAN_FOLDER = r'AutoPlans'
+PATH_PROTOCOLS = os.path.join(os.path.dirname(__file__),
+                              PROTOCOL_FOLDER, INSTITUTION_FOLDER, AUTOPLAN_FOLDER)
+PATH_TO_OUTPUT = os.path.normpath(
+    "Q:\\RadOnc\\RayStation\\RayScripts\\AutoPlanData")
+DICOM_PATH = os.path.normpath('\\\\m-rayscon02587\\DicomImageStorage')
+PROTOCOL_FILE_TOMO = "TomoTBI.xml"
+PROTOCOL_NAME_TOMO = "UW Tomo TBI"
+ORDER_NAME_FFS_TOMO = "TomoTBI_FFS"
+ORDER_NAME_HFS_TOMO = "TomoTBI_HFS"
+ORDER_NAME_HFS_KIDNEY_TOMO = "TomoTBI_Kidney_HFS"
+BEAMSET_TEMPLATE_FFS_TOMO = "Tomo_TBI_FFS_FW50"
+BEAMSET_TEMPLATE_HFS_TOMO = "Tomo_TBI_HFS_FW50"
+TOMO_MACHINE = "HDA0488"
+PROTOCOL_FILE_VMAT = "UW_VMAT_TBI.xml"
+PROTOCOL_NAME_VMAT = "UW VMAT TBI"
+BEAMSET_HFS_VMAT = "VMAT-HFS-TBI"
+BEAMSET_FFS_VMAT = "VMAT-FFS-TBI"
+VMAT_MACHINE = "TrueBeam"
+HFS_PELVIS_ORDER_NAME = 'VMAT_TBI_HFS_PELVIS_UPDATED'
+HFS_PELVIS_KIDNEY_ORDER_NAME = 'VMAT_TBI_HFS_PELVIS_KIDNEY'
+HFS_CHEST_ORDER_NAME = 'VMAT_TBI_HFS_CHEST_NOOBJ'
+HFS_HEAD_ORDER_NAME = 'VMAT_TBI_HFS_HEAD_NOOBJ'
+FFS_PELVIS_ORDER_NAME = 'VMAT_TBI_FFS_PELVIS_UPDATED'
+FFS_LEGS_ORDER_NAME = 'VMAT_TBI_FFS_LEGS_NOOBJ'
+FFS_FEET_ORDER_NAME = 'VMAT_TBI_FFS_FEET_NOOBJ'
+ORDER_TARGET_NAME_FFS = "PTV_p_FFS"
+ORDER_TARGET_NAME_HFS = "PTV_p_HFS"
+DEFAULT_VOXEL_SIZE = {'x': 0.4, 'y': 0.4, 'z': 0.4}  # [cm]
+HFS_TOMO_PLAN_NAME = "HFS__TBI_Tomo_Auto"
+HFS_TOMO_BEAMSET_NAME = "HFS__TBI_Tomo"
+FFS_TOMO_BEAMSET_NAME = "FFS__TBI_Tomo"
+FFS_TOMO_PLAN_NAME = "FFS__TBI_Tomo_Auto"
+TOMO_FFS_TRANSFER_NAME = "Tomo_FFS_Trnsfr"
+FFS_PLACEHOLDER_NAME = "Empty plan"  # Default name assigned by RS upon plan import
+HFS_VMAT_BEAMSET_NAME = "HFS__VMA"
+HFS_VMAT_PLAN_NAME = HFS_VMAT_BEAMSET_NAME + "_Auto"
+FFS_VMAT_BEAMSET_NAME = "FFS__VMA"
+FFS_VMAT_PLAN_NAME = FFS_VMAT_BEAMSET_NAME + "_Auto"
+VMAT_FFS_TRANSFER_NAME = "VMAT_FFS_Trnsfr"
+MIN_FFS_OVERLAP = 2  # Minimum Overlap
+HFS_OVERLAP = 5  # Minimum Overlap
+FW = 39  # 39 cm of MLC based field
+CENTRAL_JUNCTION_WIDTH = 1.2 * 9
+FFS_MAX_TREATMENT_LENGTH = 99  # TODO - A fudge - based junction placement on packing HFS
+FFS_OVERSHOOT = 3  # cm - Distance of overshoot of beam past toes
+FFS_SHIFT_BUFFER = 2
+FFS_TREATMENT_LENGTH = (FFS_MAX_TREATMENT_LENGTH
+                        - FFS_OVERSHOOT
+                        - FFS_SHIFT_BUFFER
+                        - CENTRAL_JUNCTION_WIDTH)
+FFS_ISO_NUMBER = math.ceil(FFS_MAX_TREATMENT_LENGTH / (FW - MIN_FFS_OVERLAP))
+HFS_MAX_TREATMENT_LENGTH = 114.5
+HFS_SHIFT_BUFFER = 2
+HFS_OVERSHOOT = 3  # cm - Distance of overshoot of beam past top of head
+HFS_TREATMENT_LENGTH = (HFS_MAX_TREATMENT_LENGTH
+                        + HFS_OVERSHOOT
+                        + HFS_SHIFT_BUFFER
+                        + CENTRAL_JUNCTION_WIDTH)
+JUNCTION_POINT = "junction"
+HFS_POI = 'HFS_POI'
+FFS_POI = 'FFS_POI'
+EXTERNAL_SETUP = 'External_PRV10'
+EXTERNAL_SETUP_EXP = 1.0  # cm expansion
+EXTERNAL_NAME = "ExternalClean"
+AVOID_HFS_NAME = "Avoid_HFS"
+AVOID_FFS_NAME = "Avoid_FFS"
+SKIN_AVOIDANCE = 'Avoid_Skin_PRV05'
+SKIN_AVOIDANCE_CONTRACT = 0.5  # cm contraction
+LUNG_AVOID_NAME = "Lungs_m07"
+LUNG_AVOID_MARGIN = 0.7  # cm contraction
+LUNGS = "Lungs"
+LUNGS_EVAL_MARGIN = 1.0  # cm contraction for margin
+LUNGS_EVAL_NAME = LUNGS + "_m10"
+KIDNEYS = "Kidneys"
+KIDNEY_AVOID_NAME = KIDNEYS + "_m05"
+KIDNEY_AVOID_MARGIN = 0.5  # cm contraction
+TARGET_FFS = "PTV_p_FFS"
+TARGET_HFS = "PTV_p_HFS"
+EVAL_SUFFIX = "_Eval"
+JUNCTION_PREFIX_FFS = "ffs_junction_"
+JUNCTION_PREFIX_HFS = "hfs_junction_"
+HFS_TARGET_EVAL_NAME = TARGET_HFS + EVAL_SUFFIX
+FFS_TARGET_EVAL_NAME = TARGET_FFS + EVAL_SUFFIX
+HFS_TARGET_NAMES = [TARGET_HFS, HFS_TARGET_EVAL_NAME]
+FFS_TARGET_NAMES = [TARGET_FFS, FFS_TARGET_EVAL_NAME]
+MBS_ROIS = {'Kidney_L': {'CaseType': "Abdomen",
+                         'ModelName': r"Kidney (Left)",
+                         'RoiName': r"Kidney_L",
+                         'RoiColor': "58, 251, 170"},
+            'Kidney_R': {'CaseType': "Abdomen",
+                         'ModelName': r"Kidney (Right)",
+                         'RoiName': r"Kidney_R",
+                         'RoiColor': "250, 57, 105"},
+            'Lung_L': {'CaseType': "Thorax",
+                       'ModelName': r"Lung (Left)",
+                       'RoiName': r"Lung_L",
+                       'RoiColor': "253, 122, 9"},
+            'Lung_R': {'CaseType': "Thorax",
+                       'ModelName': r"Lung (Right)",
+                       'RoiName': r"Lung_R",
+                       'RoiColor': "54, 247, 223"}}
+COLORS = [[127, 0, 255],
+          [0, 0, 255],
+          [0, 127, 255],
+          [0, 255, 255],
+          [0, 255, 127],
+          [0, 255, 0],
+          [127, 255, 0],
+          [255, 255, 0],
+          [255, 127, 0],
+          [255, 0, 0],
+          [255, 0, 255]]
