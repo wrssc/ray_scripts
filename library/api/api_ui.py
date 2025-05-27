27749136878a01a087fe9252288b81276c4585e1
Adam Bayliiss <37907358+rabayliss@users.noreply.github.com>
2025-05-27 16:55:56 -0500

    ✨ electron_qa_plan.py, api_beamsets.py, api_ui.py:
    Update to support RS 15 and maintain compatibility with RS 11B.
    
    - Updated `electron_qa_plan.py`:
      - Incremented version to 1.2.0.
      - Updated RayStation version to 2024A.
      - Updated copyright year to 2025.
      - Replaced direct function calls with new API imports `ui_click_qa_preparation` and `create_electron_qa_plan`.
      - Adjusted the parameters for `create_electron_qa_plan` to include `uncertainty` for RS 15 compatibility.
    
    - Modified `api_beamsets.py`:
      - Added functions `create_electron_qa_plan_v12` and `create_electron_qa_plan_v15` for version-specific QA plan creation.
      - Registered these functions with a dispatcher for version-specific handling.
      - Introduced a dispatcher function `create_electron_qa_plan` for unified API access.
    
    - Enhanced `api_ui.py`:
      - Added functions `ui_click_qa_preparation_v12` and `ui_click_qa_preparation_v15` for version-specific UI interactions.
      - Registered these functions with a dispatcher for version-specific handling.
      - Introduced a dispatcher function `ui_click_qa_preparation` for unified API access.

--- library/api/api_ui.py
+++ library/api/api_ui.py
@@ -20,6 +20,22 @@ def ui_click_plan_optimization_v15(ui):
         logging.debug(f'Unable to change viewing windows: {e}')
 
 
+def ui_click_qa_preparation_v12(ui):
+    # Version 12 specific code
+    try:
+        ui.TitleBar.Navigation.MenuItem['QA preparation'].Button_QA_preparation.Click()
+    except Exception as e:
+        logging.debug(f'Unable to change TitleBar-Navigation-MenuItem-QA preparation: {e}')
+
+
+def ui_click_qa_preparation_v15(ui):
+    # Version 15 specific code
+    try:
+        ui.TitleBar.Navigation.MenuItem['QA preparation'].Button.Click()
+    except Exception as e:
+        logging.debug(f'Unable to change TitleBar-Navigation-MenuItem-QA preparation: {e}')
+
+
 def ui_click_plan_design_v12(ui):
     # Version 12 specific code
     try:
@@ -76,12 +92,14 @@ def get_toolpanel_options_map_v15(ui):
 # REGISTER THESE FUNCTIONS WITH THE DISPATCHER
 dispatcher.register('ui_click_plan_optimization', 12, ui_click_plan_optimization_v12)
 dispatcher.register('ui_click_plan_design', 12, ui_click_plan_design_v12)
+dispatcher.register('ui_click_qa_preparation', 12, ui_click_qa_preparation_v12)
 dispatcher.register('get_toolpanel_options_map', 12, get_toolpanel_options_map_v12)
 dispatcher.register('click_apply_clinic_defaults', 12, click_apply_clinic_defaults_v12)
 
 # Temporarily map V12 to V15 until the V15 functions are determined necessary
 dispatcher.register('ui_click_plan_optimization', 15, ui_click_plan_optimization_v15)
 dispatcher.register('ui_click_plan_design', 15, ui_click_plan_design_v15)
+dispatcher.register('ui_click_qa_preparation', 15, ui_click_qa_preparation_v15)
 dispatcher.register('get_toolpanel_options_map', 15, get_toolpanel_options_map_v15)
 dispatcher.register('click_apply_clinic_defaults', 15, click_apply_clinic_defaults_v15)
 
@@ -93,6 +111,11 @@ def ui_click_plan_optimization(ui):
 
 @dispatcher.dispatch('ui_click_plan_design')
 def ui_click_plan_design(ui):
+    pass
+
+
+@dispatcher.dispatch('ui_click_qa_preparation')
+def ui_click_qa_preparation(ui):
     pass
 
 
