47a0b81c8df01e552f7eda792863efd7d8e4f1f3
Adam Bayliss <37907358+rabayliss@users.noreply.github.com>
2025-10-10 10:31:15 -0500

    🔧 DicomExport.py: Update DoseReferenceStructureType handling
    Add the modified DoseReferenceStructureType to the export list when applicable.
    
    - Added the modified DoseReferenceStructureType to the export list.
    - Ensured that the structure type is set to 'SITE' if not already specified.

--- library/DicomExport.py
+++ library/DicomExport.py
@@ -1952,6 +1952,7 @@ def delete_reference_point_location(ds, beamset, expected) -> str:
         if dose_reference_structure_type is not None:
             if dose_reference_structure_type != 'SITE':
                 drs[drs_type_tag].value = 'SITE'
+                e.add(drs[drs_type_tag])
         else:
             m = "No DoseReferenceSequence found to modify DoseReferenceStructureType"
         return m
